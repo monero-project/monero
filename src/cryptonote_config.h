26292c5cd06c3ab4250729c38fbe68ffb179787d
monero-project <sempre.amaro@gmail.com>
2014-05-26 23:03:02 -0400

    raise min fee and correct COIN value

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -26,8 +26,8 @@
 #define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
 #define CRYPTONOTE_DISPLAY_DECIMAL_POINT                12
 // COIN - number of smallest units in one coin
-#define COIN                                            ((uint64_t)100000000) // pow(10, 8)
-#define DEFAULT_FEE                                     ((uint64_t)1000000) // pow(10, 6)
+#define COIN                                            ((uint64_t)1000000000000) // pow(10, 12)
+#define DEFAULT_FEE                                     ((uint64_t)5000000000) // 5 * pow(10, 9)
 
 
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
