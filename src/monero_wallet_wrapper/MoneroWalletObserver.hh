2ee13e7ea65a9ef8039ff51133f588aa1283af61
Neozaru <neozaru@mailoo.org>
2014-06-20 22:56:15 +0200

    Observer callbacks are not pure anymore

--- src/monero_wallet_wrapper/MoneroWalletObserver.hh
+++ src/monero_wallet_wrapper/MoneroWalletObserver.hh
@@ -9,17 +9,17 @@ class WalletObserver
 {
 public:
 
-    virtual void on_new_block(uint64_t height) = 0;
+    virtual void on_new_block(uint64_t height) {}
 
-    virtual void on_money_received(uint64_t height, const std::string& transaction_id, Monero::amount_mini_t amount_mini) = 0;
+    virtual void on_money_received(uint64_t height, const std::string& transaction_id, Monero::amount_mini_t amount_mini) {}
 
-    virtual void on_money_spent(uint64_t height, const std::string& transaction_id, Monero::amount_mini_t amount_mini) = 0;
+    virtual void on_money_spent(uint64_t height, const std::string& transaction_id, Monero::amount_mini_t amount_mini) {}
 
-    virtual void on_skip_transaction(uint64_t height, const std::string& transaction_id) = 0;
+    virtual void on_skip_transaction(uint64_t height, const std::string& transaction_id) {}
 
-    virtual void on_payment_received(uint64_t height, const std::string& payment_id, const Payment& payment) = 0;
+    virtual void on_payment_received(uint64_t height, const std::string& payment_id, const Payment& payment) {}
 
-    virtual void on_wallet_refreshed(Monero::Wallet* wallet, size_t fetched_blocks_count, bool hasReceivedMoney) = 0;
+    virtual void on_wallet_refreshed(Monero::Wallet* wallet, size_t fetched_blocks_count, bool hasReceivedMoney) {}
 
 };
 
