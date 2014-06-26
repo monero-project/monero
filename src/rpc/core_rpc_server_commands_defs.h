9ddccbde63665e4f7bffdf2cb47a5cb5711686e7
Zachary Michaels <mikezackles@gmail.com>
2014-06-26 17:16:22 -0400

    Move tx_info into its own file

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -8,6 +8,7 @@
 #include "cryptonote_core/difficulty.h"
 #include "crypto/hash.h"
 #include "rpc/block_header_responce.h"
+#include "rpc/tx_info.h"
 #include "p2p/connection_info.h"
 
 namespace cryptonote
@@ -531,58 +532,6 @@ namespace cryptonote
         KV_SERIALIZE(status)
       END_KV_SERIALIZE_MAP()
     };
-  };
-
-  struct tx_info
-  {
-    std::string id_hash;
-    std::string tx_json; // TODO - expose this data directly
-    uint64_t blob_size;
-    uint64_t fee;
-    std::string max_used_block_id_hash;
-    uint64_t max_used_block_height;
-    bool kept_by_block;
-    uint64_t last_failed_height;
-    std::string last_failed_id_hash;
-    uint64_t receive_time;
-
-    tx_info() = default;
-
-    tx_info(
-          std::string id_hash
-        , std::string tx_json
-        , uint64_t blob_size
-        , uint64_t fee
-        , std::string max_used_block_id_hash
-        , uint64_t max_used_block_height
-        , bool kept_by_block
-        , uint64_t last_failed_height
-        , std::string last_failed_id_hash
-        , uint64_t receive_time
-        )
-      : id_hash(std::move(id_hash))
-      , tx_json(tx_json)
-      , blob_size(blob_size)
-      , fee(fee)
-      , max_used_block_id_hash(std::move(max_used_block_id_hash))
-      , kept_by_block(kept_by_block)
-      , last_failed_height(last_failed_height)
-      , last_failed_id_hash(std::move(last_failed_id_hash))
-      , receive_time(receive_time)
-    {}
-
-    BEGIN_KV_SERIALIZE_MAP()
-      KV_SERIALIZE(id_hash)
-      KV_SERIALIZE(tx_json)
-      KV_SERIALIZE(blob_size)
-      KV_SERIALIZE(fee)
-      KV_SERIALIZE(max_used_block_id_hash)
-      KV_SERIALIZE(max_used_block_height)
-      KV_SERIALIZE(kept_by_block)
-      KV_SERIALIZE(last_failed_height)
-      KV_SERIALIZE(last_failed_id_hash)
-      KV_SERIALIZE(receive_time)
-    END_KV_SERIALIZE_MAP()
   };
 
   struct COMMAND_RPC_GET_TRANSACTION_POOL
