aaced6ff9bf53c339b2016f39994fa7dbc9b6044
Zachary Michaels <mikezackles@gmail.com>
2014-06-27 13:29:32 -0400

    Add missing pragma

--- src/serialization/json_utils.h
+++ src/serialization/json_utils.h
@@ -2,6 +2,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#pragma once
+
 #include <sstream>
 #include "json_archive.h"
 
