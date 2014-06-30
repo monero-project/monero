28af03208bc38eeaa3e903e336412d221571bb92
Zachary Michaels <mikezackles@gmail.com>
2014-06-30 15:26:29 -0400

    Remove redeclaration

--- src/cryptonote_core/cryptonote_format_utils.h
+++ src/cryptonote_core/cryptonote_format_utils.h
@@ -85,7 +85,6 @@ namespace cryptonote
   uint64_t get_outs_money_amount(const transaction& tx);
   bool check_inputs_types_supported(const transaction& tx);
   bool check_outs_valid(const transaction& tx);
-  blobdata get_block_hashing_blob(const block& b);
   bool parse_amount(uint64_t& amount, const std::string& str_amount);
 
   bool check_money_overflow(const transaction& tx);
