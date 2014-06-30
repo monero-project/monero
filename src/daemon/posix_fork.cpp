06059b87fd8eeaaf5b748142af9d06b576d4e827
Zachary Michaels <mikezackles@gmail.com>
2014-06-30 10:46:54 -0400

    Add missing boost license

--- src/daemon/posix_fork.cpp
+++ src/daemon/posix_fork.cpp
@@ -1,3 +1,13 @@
+//
+// daemon.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include "daemon/posix_fork.h"
 
 #include <cstdlib>
