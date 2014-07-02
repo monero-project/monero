f9e56ec1f11f9d980484241ff385dec8a665428f
Zachary Michaels <mikezackles@gmail.com>
2014-07-02 10:35:53 -0400

    Fix license

--- src/daemon/posix_fork.cpp
+++ src/daemon/posix_fork.cpp
@@ -1,7 +1,3 @@
-//
-// daemon.cpp
-// ~~~~~~~~~~
-//
 // Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
