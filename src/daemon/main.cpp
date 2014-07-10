352873c0b45fc6d4a854159fbd2e7a6531e96199
Zachary Michaels <mikezackles@gmail.com>
2014-07-10 14:07:22 -0400

    Remove macros

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -69,222 +69,231 @@ namespace
 
 int main(int argc, char const * argv[])
 {
-  epee::string_tools::set_module_name_and_folder(argv[0]);
-  TRY_ENTRY();
+  try {
 
-  // Build argument description
-  po::options_description all_options("All");
-  po::options_description visible_options("Options");
-  po::options_description core_settings("Settings");
-  po::positional_options_description positional;
-  {
-    bf::path default_data_dir = bf::absolute(tools::get_default_data_dir());
+    epee::string_tools::set_module_name_and_folder(argv[0]);
 
-    // Misc Options
-
-    command_line_options::init_help_option(visible_options);
-    command_line_options::init_system_query_options(visible_options);
-    command_line::add_arg(visible_options, command_line::arg_data_dir, default_data_dir.string());
-    command_line::add_arg(visible_options, arg_config_file, std::string(CRYPTONOTE_NAME ".conf"));
-    command_line::add_arg(visible_options, arg_detach);
-
-    // Settings
-    command_line::add_arg(core_settings, arg_log_file, std::string(CRYPTONOTE_NAME ".log"));
-    command_line::add_arg(core_settings, arg_log_level);
-    daemonize::t_daemon::init_options(core_settings);
-
-    // Hidden options
-    command_line::add_arg(all_options, arg_command);
-#   ifdef WIN32
-      command_line::add_arg(all_options, arg_windows_service);
-#   endif
-
-    visible_options.add(core_settings);
-    all_options.add(visible_options);
-
-    // Positional
-    positional.add(arg_command.name, -1); // -1 for unlimited arguments
-  }
-
-  // Do command line parsing
-  po::variables_map vm;
-  if (!command_line_options::parse_options(vm, argc, argv, visible_options, all_options, positional))
-  {
-    return 1;
-  }
-
-  if (command_line_options::print_help(
-        "Usage: " + std::string{argv[0]} + " [options|settings] [daemon_command...]"
-      , vm, visible_options
-      ))
-  {
-    return 0;
-  }
-
-  if (command_line_options::query_system_info(vm))
-  {
-    return 0;
-  }
-
-  // Try to create requested/defaulted data directory
-  {
-    bf::path data_dir = bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir));
-    tools::create_directories_if_necessary(data_dir.string());
-  }
-
-  // Parse config file if it exists
-  {
-    bf::path data_dir_path(bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir)));
-    bf::path config_path(command_line::get_arg(vm, arg_config_file));
-
-    if (config_path.is_relative())
+    // Build argument description
+    po::options_description all_options("All");
+    po::options_description visible_options("Options");
+    po::options_description core_settings("Settings");
+    po::positional_options_description positional;
     {
-      config_path = data_dir_path / config_path;
+      bf::path default_data_dir = bf::absolute(tools::get_default_data_dir());
+
+      // Misc Options
+
+      command_line_options::init_help_option(visible_options);
+      command_line_options::init_system_query_options(visible_options);
+      command_line::add_arg(visible_options, command_line::arg_data_dir, default_data_dir.string());
+      command_line::add_arg(visible_options, arg_config_file, std::string(CRYPTONOTE_NAME ".conf"));
+      command_line::add_arg(visible_options, arg_detach);
+
+      // Settings
+      command_line::add_arg(core_settings, arg_log_file, std::string(CRYPTONOTE_NAME ".log"));
+      command_line::add_arg(core_settings, arg_log_level);
+      daemonize::t_daemon::init_options(core_settings);
+
+      // Hidden options
+      command_line::add_arg(all_options, arg_command);
+#     ifdef WIN32
+        command_line::add_arg(all_options, arg_windows_service);
+#     endif
+
+      visible_options.add(core_settings);
+      all_options.add(visible_options);
+
+      // Positional
+      positional.add(arg_command.name, -1); // -1 for unlimited arguments
     }
 
-    boost::system::error_code ec;
-    if (bf::exists(config_path, ec))
+    // Do command line parsing
+    po::variables_map vm;
+    if (!command_line_options::parse_options(vm, argc, argv, visible_options, all_options, positional))
     {
-      po::store(po::parse_config_file<char>(config_path.string<std::string>().c_str(), core_settings), vm);
-    }
-    po::notify(vm);
-  }
-
-  // If there are positional options, we're running a daemon command
-  if (command_line::arg_present(vm, arg_command))
-  {
-    auto command = command_line::get_arg(vm, arg_command);
-    auto rpc_ip_str = command_line::get_arg(vm, cryptonote::core_rpc_server::arg_rpc_bind_ip);
-    auto rpc_port_str = command_line::get_arg(vm, cryptonote::core_rpc_server::arg_rpc_bind_port);
-
-    uint32_t rpc_ip;
-    uint16_t rpc_port;
-    if (!epee::string_tools::get_ip_int32_from_string(rpc_ip, rpc_ip_str))
-    {
-      std::cerr << "Invalid IP: " << rpc_ip_str << std::endl;
-      return 1;
-    }
-    if (!epee::string_tools::get_xtype_from_string(rpc_port, rpc_port_str))
-    {
-      std::cerr << "Invalid port: " << rpc_port_str << std::endl;
       return 1;
     }
 
-    daemonize::t_command_server rpc_commands{rpc_ip, rpc_port};
-    if (rpc_commands.process_command_vec(command))
+    if (command_line_options::print_help(
+          "Usage: " + std::string{argv[0]} + " [options|settings] [daemon_command...]"
+        , vm, visible_options
+        ))
     {
       return 0;
     }
-    else
+
+    if (command_line_options::query_system_info(vm))
     {
-      std::cerr << "Unknown command" << std::endl;
-      return 1;
-    }
-  }
-
-  // Start with log level 0
-  epee::log_space::get_set_log_detalisation_level(true, LOG_LEVEL_0);
-
-  // Set log level
-  {
-    int new_log_level = command_line::get_arg(vm, arg_log_level);
-    if(new_log_level < LOG_LEVEL_MIN || new_log_level > LOG_LEVEL_MAX)
-    {
-      LOG_PRINT_L0("Wrong log level value: " << new_log_level);
-    }
-    else if (epee::log_space::get_set_log_detalisation_level(false) != new_log_level)
-    {
-      epee::log_space::get_set_log_detalisation_level(true, new_log_level);
-      LOG_PRINT_L0("LOG_LEVEL set to " << new_log_level);
-    }
-  }
-
-  bool detach = command_line::arg_present(vm, arg_detach);
-  bool win_service = command_line::arg_present(vm, arg_windows_service);
-
-  // Set log file
-  {
-    bf::path data_dir{bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir))};
-    bf::path log_file_path{command_line::get_arg(vm, arg_log_file)};
-
-    if (log_file_path.is_relative())
-    {
-      log_file_path = data_dir / log_file_path;
+      return 0;
     }
 
-    // Fall back to relative path for log file
-    boost::system::error_code ec;
-    if (!log_file_path.has_parent_path() || !bf::exists(log_file_path.parent_path(), ec))
+    // Try to create requested/defaulted data directory
     {
-      log_file_path = epee::log_space::log_singletone::get_default_log_file();
+      bf::path data_dir = bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir));
+      tools::create_directories_if_necessary(data_dir.string());
     }
 
-    std::string log_dir;
-    log_dir = log_file_path.has_parent_path() ? log_file_path.parent_path().string() : epee::log_space::log_singletone::get_default_log_folder();
-    epee::log_space::log_singletone::add_logger(LOGGER_FILE, log_file_path.filename().string().c_str(), log_dir.c_str());
-  }
+    // Parse config file if it exists
+    {
+      bf::path data_dir_path(bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir)));
+      bf::path config_path(command_line::get_arg(vm, arg_config_file));
 
-  if (!detach && !win_service)
-  {
-    epee::log_space::log_singletone::add_logger(LOGGER_CONSOLE, NULL, NULL);
-  }
-
-  /**
-   * Windows
-   * -------
-   *
-   * If detach is requested, we ask Windows to relaunch the executable as a
-   * service with the added --run-as-service argument, which indicates that the
-   * process is running in the background.
-   *
-   * On relaunch the --run-as-service argument is detected, and the
-   * t_service_runner class finishes registering as a service and installs the
-   * required service lifecycle handler callback.
-   *
-   * Posix
-   * -----
-   *
-   * Much simpler.  We just fork if detach is requested.
-   */
-  if (win_service) // running as windows service
-  {
-#   ifdef WIN32
-      LOG_PRINT_L0(CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG);
-      windows::t_service_runner<daemonize::t_daemon>::run(
-        WINDOWS_SERVICE_NAME
-      , daemonize::t_daemon{vm}
-      );
-#   endif
-  }
-  else if (detach)
-  {
-#   ifdef WIN32
-      // install windows service
-      std::string arguments = get_argument_string(argc, argv) + " --run-as-service";
-      bool install = windows::install_service(WINDOWS_SERVICE_NAME, arguments);
-      bool start = false;
-      if (install)
+      if (config_path.is_relative())
       {
-        start = windows::start_service(WINDOWS_SERVICE_NAME);
+        config_path = data_dir_path / config_path;
       }
-      if (install && !start)
+
+      boost::system::error_code ec;
+      if (bf::exists(config_path, ec))
       {
-        windows::uninstall_service(WINDOWS_SERVICE_NAME);
+        po::store(po::parse_config_file<char>(config_path.string<std::string>().c_str(), core_settings), vm);
       }
-#   else
-      // fork
-      posix::fork();
+      po::notify(vm);
+    }
+
+    // If there are positional options, we're running a daemon command
+    if (command_line::arg_present(vm, arg_command))
+    {
+      auto command = command_line::get_arg(vm, arg_command);
+      auto rpc_ip_str = command_line::get_arg(vm, cryptonote::core_rpc_server::arg_rpc_bind_ip);
+      auto rpc_port_str = command_line::get_arg(vm, cryptonote::core_rpc_server::arg_rpc_bind_port);
+
+      uint32_t rpc_ip;
+      uint16_t rpc_port;
+      if (!epee::string_tools::get_ip_int32_from_string(rpc_ip, rpc_ip_str))
+      {
+        std::cerr << "Invalid IP: " << rpc_ip_str << std::endl;
+        return 1;
+      }
+      if (!epee::string_tools::get_xtype_from_string(rpc_port, rpc_port_str))
+      {
+        std::cerr << "Invalid port: " << rpc_port_str << std::endl;
+        return 1;
+      }
+
+      daemonize::t_command_server rpc_commands{rpc_ip, rpc_port};
+      if (rpc_commands.process_command_vec(command))
+      {
+        return 0;
+      }
+      else
+      {
+        std::cerr << "Unknown command" << std::endl;
+        return 1;
+      }
+    }
+
+    // Start with log level 0
+    epee::log_space::get_set_log_detalisation_level(true, LOG_LEVEL_0);
+
+    // Set log level
+    {
+      int new_log_level = command_line::get_arg(vm, arg_log_level);
+      if(new_log_level < LOG_LEVEL_MIN || new_log_level > LOG_LEVEL_MAX)
+      {
+        LOG_PRINT_L0("Wrong log level value: " << new_log_level);
+      }
+      else if (epee::log_space::get_set_log_detalisation_level(false) != new_log_level)
+      {
+        epee::log_space::get_set_log_detalisation_level(true, new_log_level);
+        LOG_PRINT_L0("LOG_LEVEL set to " << new_log_level);
+      }
+    }
+
+    bool detach = command_line::arg_present(vm, arg_detach);
+    bool win_service = command_line::arg_present(vm, arg_windows_service);
+
+    // Set log file
+    {
+      bf::path data_dir{bf::absolute(command_line::get_arg(vm, command_line::arg_data_dir))};
+      bf::path log_file_path{command_line::get_arg(vm, arg_log_file)};
+
+      if (log_file_path.is_relative())
+      {
+        log_file_path = data_dir / log_file_path;
+      }
+
+      // Fall back to relative path for log file
+      boost::system::error_code ec;
+      if (!log_file_path.has_parent_path() || !bf::exists(log_file_path.parent_path(), ec))
+      {
+        log_file_path = epee::log_space::log_singletone::get_default_log_file();
+      }
+
+      std::string log_dir;
+      log_dir = log_file_path.has_parent_path() ? log_file_path.parent_path().string() : epee::log_space::log_singletone::get_default_log_folder();
+      epee::log_space::log_singletone::add_logger(LOGGER_FILE, log_file_path.filename().string().c_str(), log_dir.c_str());
+    }
+
+    if (!detach && !win_service)
+    {
+      epee::log_space::log_singletone::add_logger(LOGGER_CONSOLE, NULL, NULL);
+    }
+
+    /**
+     * Windows
+     * -------
+     *
+     * If detach is requested, we ask Windows to relaunch the executable as a
+     * service with the added --run-as-service argument, which indicates that the
+     * process is running in the background.
+     *
+     * On relaunch the --run-as-service argument is detected, and the
+     * t_service_runner class finishes registering as a service and installs the
+     * required service lifecycle handler callback.
+     *
+     * Posix
+     * -----
+     *
+     * Much simpler.  We just fork if detach is requested.
+     */
+    if (win_service) // running as windows service
+    {
+#     ifdef WIN32
+        LOG_PRINT_L0(CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG);
+        windows::t_service_runner<daemonize::t_daemon>::run(
+          WINDOWS_SERVICE_NAME
+        , daemonize::t_daemon{vm}
+        );
+#     endif
+    }
+    else if (detach)
+    {
+#     ifdef WIN32
+        // install windows service
+        std::string arguments = get_argument_string(argc, argv) + " --run-as-service";
+        bool install = windows::install_service(WINDOWS_SERVICE_NAME, arguments);
+        bool start = false;
+        if (install)
+        {
+          start = windows::start_service(WINDOWS_SERVICE_NAME);
+        }
+        if (install && !start)
+        {
+          windows::uninstall_service(WINDOWS_SERVICE_NAME);
+        }
+#     else
+        // fork
+        posix::fork();
+        LOG_PRINT_L0(CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG);
+        daemonize::t_daemon{vm}.run();
+#     endif
+    }
+    else // interactive
+    {
       LOG_PRINT_L0(CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG);
       daemonize::t_daemon{vm}.run();
-#   endif
+    }
+
+    return 0;
   }
-  else // interactive
+  catch (std::exception const & ex)
   {
-    LOG_PRINT_L0(CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG);
-    daemonize::t_daemon{vm}.run();
+    LOG_ERROR("Exception in main! " << ex.what());
   }
-
-  return 0;
-
-  CATCH_ENTRY_L0("main", 1);
+  catch (...)
+  {
+    LOG_ERROR("Exception in main!");
+  }
+  return 1;
 }
