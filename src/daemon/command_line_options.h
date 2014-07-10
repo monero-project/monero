d21ba181f30d195e8255b1b77943dccc482dbf7f
Zachary Michaels <mikezackles@gmail.com>
2014-07-10 14:17:10 -0400

    Format

--- src/daemon/command_line_options.h
+++ src/daemon/command_line_options.h
@@ -7,9 +7,11 @@ namespace command_line_options
   void init_help_option(
       boost::program_options::options_description & option_spec
     );
+
   void init_system_query_options(
       boost::program_options::options_description & option_spec
     );
+
   bool parse_options(
       boost::program_options::variables_map & result
     , int argc, char const * argv[]
@@ -17,11 +19,13 @@ namespace command_line_options
     , boost::program_options::options_description const & all_options
     , boost::program_options::positional_options_description const & positional_options
     );
+
   bool print_help(
       std::string const & usage_note
     , boost::program_options::variables_map const & vm
     , boost::program_options::options_description const & visible_options
     );
+
   bool query_system_info(
       boost::program_options::variables_map const & vm
     );
