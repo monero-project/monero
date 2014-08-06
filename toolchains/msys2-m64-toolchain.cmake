ceb6977f82328747d9e97cbcbdd064275ae6abab
Zachary Michaels <mikezackles@gmail.com>
2014-08-06 13:13:06 -0400

    msys2 32/64-bit toolchains

---
+++ toolchains/msys2-m64-toolchain.cmake
@@ -0,0 +1,18 @@
+set (CMAKE_SYSTEM_NAME Windows)
+
+set (GCC_PREFIX x86_64-w64-mingw32)
+set (CMAKE_C_COMPILER ${GCC_PREFIX}-gcc)
+set (CMAKE_CXX_COMPILER ${GCC_PREFIX}-g++)
+set (CMAKE_AR ${GCC_PREFIX}-gcc-ar CACHE FILEPATH "" FORCE)
+set (CMAKE_NM ${GCC_PREFIX}-gcc-nm CACHE FILEPATH "" FORCE)
+set (CMAKE_RANLIB ${GCC_PREFIX}-gcc-ranlib CACHE FILEPATH "" FORCE)
+set (CMAKE_RC_COMPILER windres)
+
+set (CMAKE_FIND_ROOT_PATH c:/msys2/mingw64)
+
+# Ensure cmake doesn't find things in the wrong places
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) # Find programs on host
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY) # Find libs in target
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY) # Find includes in target
+
+set (MINGW_FLAG "-m64")
