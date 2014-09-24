22481244535e05bb308eedf9c2d2a5abe2d79a9d
Thomas Winget <tewinget@gmail.com>
2014-09-24 17:15:49 -0400

    Removed ldns dependency
    ldns dependency was only still around for constants defined in ldns/rr.h,
    but those constants are RFC specified DNS constants, and to reduce deps
    have been replicated in dns_utils.h instead of including ldns/rr.h.

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -29,7 +29,6 @@
 #include "common/dns_utils.h"
 #include <cstring>
 #include <sstream>
-#include <ldns/rr.h> // for RR type and class defs
 #include <unbound.h>
 
 namespace tools
@@ -135,7 +134,7 @@ std::vector<std::string> DNSResolver::get_ipv4(const std::string& url)
   ub_result_ptr result;
 
   // call DNS resolver, blocking.  if return value not zero, something went wrong
-  if (!ub_resolve(m_data->m_ub_context, urlC, LDNS_RR_TYPE_A, LDNS_RR_CLASS_IN, &(result.ptr)))
+  if (!ub_resolve(m_data->m_ub_context, urlC, DNS_TYPE_A, DNS_CLASS_IN, &(result.ptr)))
   {
     if (result.ptr->havedata)
     {
@@ -165,7 +164,7 @@ std::vector<std::string> DNSResolver::get_ipv6(const std::string& url)
   ub_result_ptr result;
 
   // call DNS resolver, blocking.  if return value not zero, something went wrong
-  if (!ub_resolve(m_data->m_ub_context, urlC, LDNS_RR_TYPE_AAAA, LDNS_RR_CLASS_IN, &(result.ptr)))
+  if (!ub_resolve(m_data->m_ub_context, urlC, DNS_TYPE_AAAA, DNS_CLASS_IN, &(result.ptr)))
   {
     if (result.ptr->havedata)
     {
@@ -195,7 +194,7 @@ std::vector<std::string> DNSResolver::get_txt_record(const std::string& url)
   ub_result_ptr result;
 
   // call DNS resolver, blocking.  if return value not zero, something went wrong
-  if (!ub_resolve(m_data->m_ub_context, urlC, LDNS_RR_TYPE_TXT, LDNS_RR_CLASS_IN, &(result.ptr)))
+  if (!ub_resolve(m_data->m_ub_context, urlC, DNS_TYPE_TXT, DNS_CLASS_IN, &(result.ptr)))
   {
     if (result.ptr->havedata)
     {
