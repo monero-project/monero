edfaa2e4843cb44f42ef4834a9bc109297450006
Oran Juice <oranjuices@hotmail.com>
2014-11-01 21:35:26 +0530

    Wrote a JSON RPC HTTP Server class using net_skeleton. Has issues due to non-OOP nature of net_skeleton

---
+++ src/rpc/json_rpc_http_server.h
@@ -0,0 +1,30 @@
+#include "net_skeleton/net_skeleton.h"
+#include <boost/thread.hpp>
+#include <string>
+
+#define MAX_METHODS 100
+
+namespace RPC
+{
+  class Json_rpc_http_server
+  {
+    struct ns_mgr mgr;
+    struct ns_connection *nc;
+    boost::thread *server_thread;
+    void ev_handler(struct ns_connection *nc, int ev, void *ev_data);
+    void poll();
+    std::string m_ip;
+    std::string m_port;
+    bool m_is_running;
+    char *m_method_names[MAX_METHODS];
+    ns_rpc_handler_t m_handlers[MAX_METHODS];
+    int m_method_count;
+  public:
+    Json_rpc_http_server(const std::string &ip, const std::string &port);
+    ~Json_rpc_http_server();
+    void start();
+    void stop();
+    bool add_handler(const std::string &method_name,
+      int (*hander)(char *buf, int len, struct ns_rpc_request *req));
+  };
+}
