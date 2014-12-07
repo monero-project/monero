198368b2e1674307539d6646dac8f4ad57170469
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2014-12-07 13:17:25 +0000

    blockchain: fix wallet syncing from scratch
    When the wallet syncs from the first block, it is fine to start
    at the genesis block.

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1486,7 +1486,8 @@ bool Blockchain::find_blockchain_supplement(const std::list<crypto::hash>& qbloc
   }
 
   // if split_height remains 0, we didn't have any but the genesis block in common
-  if(split_height == 0)
+  // which is only fine if the blocks just have the genesis block
+  if(split_height == 0 && qblock_ids.size() > 1)
   {
     LOG_ERROR("Ours and foreign blockchain have only genesis block in common... o.O");
     return false;
