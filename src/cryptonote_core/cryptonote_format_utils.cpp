d91eb8c7b43fc73b9333de800500f217e370cc4b
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2015-08-19 20:59:44 +0100

    wallet: only return tx keys via RPC if requested
    To get the tx keys returned via RPC, set the "get_tx_key" or
    "get_tx_keys" request field to true (defaults to false).

--- src/cryptonote_core/cryptonote_format_utils.cpp
+++ src/cryptonote_core/cryptonote_format_utils.cpp
@@ -408,7 +408,7 @@ namespace cryptonote
     return encrypt_payment_id(payment_id, public_key, secret_key);
   }
   //---------------------------------------------------------------
-  bool construct_tx(const account_keys& sender_account_keys, const std::vector<tx_source_entry>& sources, const std::vector<tx_destination_entry>& destinations, std::vector<uint8_t> extra, transaction& tx, uint64_t unlock_time, crypto::secret_key &tx_key)
+  bool construct_tx_and_get_tx_key(const account_keys& sender_account_keys, const std::vector<tx_source_entry>& sources, const std::vector<tx_destination_entry>& destinations, std::vector<uint8_t> extra, transaction& tx, uint64_t unlock_time, crypto::secret_key &tx_key)
   {
     tx.vin.clear();
     tx.vout.clear();
@@ -576,6 +576,12 @@ namespace cryptonote
     LOG_PRINT2("construct_tx.log", "transaction_created: " << get_transaction_hash(tx) << ENDL << obj_to_json_str(tx) << ENDL << ss_ring_s.str() , LOG_LEVEL_3);
 
     return true;
+  }
+  //---------------------------------------------------------------
+  bool construct_tx(const account_keys& sender_account_keys, const std::vector<tx_source_entry>& sources, const std::vector<tx_destination_entry>& destinations, std::vector<uint8_t> extra, transaction& tx, uint64_t unlock_time)
+  {
+     crypto::secret_key tx_key;
+     return construct_tx_and_get_tx_key(sender_account_keys, sources, destinations, extra, tx, unlock_time, tx_key);
   }
   //---------------------------------------------------------------
   bool get_inputs_money_amount(const transaction& tx, uint64_t& money)
