6827061d5f5a0eb71ed6a98f1feb544954876633
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2016-01-31 22:11:57 +0000

    tests: fix compilation failure after V1 define rename

--- tests/core_tests/block_validation.cpp
+++ tests/core_tests/block_validation.cpp
@@ -505,7 +505,7 @@ bool gen_block_is_too_big::generate(std::vector<test_event_entry>& events) const
 
   // Creating a huge miner_tx, it will have a lot of outs
   MAKE_MINER_TX_MANUALLY(miner_tx, blk_0);
-  static const size_t tx_out_count = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE / 2;
+  static const size_t tx_out_count = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1 / 2;
   uint64_t amount = get_outs_money_amount(miner_tx);
   uint64_t portion = amount / tx_out_count;
   uint64_t remainder = amount % tx_out_count;
