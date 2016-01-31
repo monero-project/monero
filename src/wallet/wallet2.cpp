616df496e93b55cbff8149c836a6b68753d01e85
Riccardo Spagni <ric@spagni.net>
2016-01-31 15:49:11 +0200

    Revert "Merge pull request #632"
    This reverts commit 58f890652e86039e98dff2d48d217c9ea9e84eca, reversing
    changes made to 39d73d2a27730e6a5844c259d81a0ed9d4ee3000.

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -753,15 +753,7 @@ void wallet2::refresh(uint64_t start_height, uint64_t & blocks_fetched, bool& re
       bool error = false;
       pull_thread = std::thread([&]{pull_next_blocks(start_height, next_blocks_start_height, short_chain_history, blocks, next_blocks, error);});
 
-      try
-      {
-        process_blocks(blocks_start_height, blocks, added_blocks);
-      }
-      catch (...)
-      {
-        pull_thread.join();
-        throw;
-      }
+      process_blocks(blocks_start_height, blocks, added_blocks);
       blocks_fetched += added_blocks;
       pull_thread.join();
       if(!added_blocks)
