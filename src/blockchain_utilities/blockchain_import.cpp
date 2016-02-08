b368e29f67a856872fa7ae01b918d66d76545929
warptangent <warptangent@tutanota.com>
2016-02-08 08:12:11 -0800

    blockchain_import: Get hard fork version from HardFork
    Replace temporary assignment that used hardcoded values.

--- src/blockchain_utilities/blockchain_import.cpp
+++ src/blockchain_utilities/blockchain_import.cpp
@@ -400,11 +400,8 @@ int import_from_file(FakeCore& simple_core, const std::string& import_file_path,
             // size_t blob_size = 0;
             // get_transaction_hash(tx, hsh, blob_size);
 
-            // we'd need to get the starting heights from the daemon
-            // to be correct once voting kicks in
-            uint64_t v2height = opt_testnet ? 624634 : 1009827;
 
-            uint8_t version = h < v2height ? 1 : 2;
+            uint8_t version = simple_core.m_storage.get_current_hard_fork_version();
             tx_verification_context tvc = AUTO_VAL_INIT(tvc);
             bool r = true;
             r = simple_core.m_pool.add_tx(tx, tvc, true, true, version);
