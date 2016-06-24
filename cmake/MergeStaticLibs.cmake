beb6d9200d15e8fbaab62b90b9955b0127fc2ba9
Ilya Kitaev <mbg033@gmail.com>
2016-06-24 14:49:23 +0300

    CMake: GPL 'libutils' script removed

--- cmake/MergeStaticLibs.cmake
+++ cmake/MergeStaticLibs.cmake
@@ -1,4 +1,15 @@
+#    Copyright (C) 2012 Modelon AB
 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the BSD style license.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    FMILIB_License.txt file for more details.
+
+#    You should have received a copy of the FMILIB_License.txt file
+#    along with this program. If not, contact Modelon AB <http://www.modelon.com>.
 
 # Merge_static_libs(outlib lib1 lib2 ... libn) merges a number of static
 # libs into a single static library
