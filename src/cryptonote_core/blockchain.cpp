887db9f93670da8c1eb28c9d53d6d4f055486e3b
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2016-08-28 21:20:01 +0100

    blockchain: testnet heights for v3, v4, and v5

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -99,6 +99,10 @@ static const struct {
 
   // version 2 starts from block 624634, which is on or around the 23rd of November, 2015. Fork time finalised on 2015-11-20. No fork voting occurs for the v2 fork.
   { 2, 624634, 0, 1445355000 },
+
+  { 3, 800500, 0, 1472415034 },
+  { 4, 801220, 0, 1472415035 },
+  { 5, 802660, 0, 1472415036 },
 };
 static const uint64_t testnet_hard_fork_version_1_till = 624633;
 
