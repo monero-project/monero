c41098aacf86d4fe06958bde2b4f5093dcd59011
Riccardo Spagni <ric@spagni.net>
2016-09-18 19:18:16 +0200

    updated fork heights for v4 and v5

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -85,6 +85,12 @@ static const struct {
 
   // version 3 starts from block 1141317, which is on or around the 24th of September, 2016. Fork time finalised on 2016-03-21.
   { 3, 1141317, 0, 1458558528 },
+  
+  // version 4 starts from block 1220517, which is on or around the 5th of January, 2017. Fork time finalised on 2016-09-18.
+  { 4, 1220517, 0, 1483574400 },
+  
+  // version 5 starts from block 1406997, which is on or around the 20th of September, 2016. Fork time finalised on 2016-09-18.
+  { 5, 1141317, 0, 1505865600 },  
 };
 static const uint64_t mainnet_hard_fork_version_1_till = 1009826;
 
@@ -100,6 +106,7 @@ static const struct {
   // version 2 starts from block 624634, which is on or around the 23rd of November, 2015. Fork time finalised on 2015-11-20. No fork voting occurs for the v2 fork.
   { 2, 624634, 0, 1445355000 },
 
+  // versions 3-5 were passed in rapid succession from September 18th, 2016
   { 3, 800500, 0, 1472415034 },
   { 4, 801220, 0, 1472415035 },
   { 5, 802660, 0, 1472415036 },
