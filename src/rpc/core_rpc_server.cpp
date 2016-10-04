31ec0476543b02c5a75a38bd970559338c8b5c94
NanoAkron <nanoakron@users.noreply.github.com>
2016-10-04 13:34:13 +0100

    Missed this mis-spelling of responce/response

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -961,9 +961,9 @@ namespace cryptonote
         error_resp.message = "Internal error: coinbase transaction in the block has the wrong height";
         return false;
       }
-      res.headers.push_back(block_header_responce());
-      bool responce_filled = fill_block_header_responce(blk, false, block_height, block_hash, res.headers.back());
-      if (!responce_filled)
+      res.headers.push_back(block_header_response());
+      bool responce_filled = fill_block_header_response(blk, false, block_height, block_hash, res.headers.back());
+      if (!response_filled)
       {
         error_resp.code = CORE_RPC_ERROR_CODE_INTERNAL_ERROR;
         error_resp.message = "Internal error: can't produce valid response.";
