e59caf87d3f79e8460ee404d45c21e5010dc1042
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2016-11-04 11:11:45 +0000

    performance_tests: re-enable them...
    I think I'm the only one to review things here...

--- tests/performance_tests/main.cpp
+++ tests/performance_tests/main.cpp
@@ -52,7 +52,6 @@ int main(int argc, char** argv)
   performance_timer timer;
   timer.start();
 
-goto ree;
   TEST_PERFORMANCE3(test_construct_tx, 1, 1, false);
   TEST_PERFORMANCE3(test_construct_tx, 1, 2, false);
   TEST_PERFORMANCE3(test_construct_tx, 1, 10, false);
@@ -102,7 +101,6 @@ goto ree;
   TEST_PERFORMANCE0(test_derive_public_key);
   TEST_PERFORMANCE0(test_derive_secret_key);
   TEST_PERFORMANCE0(test_ge_frombytes_vartime);
-ree:
   TEST_PERFORMANCE0(test_generate_keypair);
 
   TEST_PERFORMANCE0(test_cn_slow_hash);
