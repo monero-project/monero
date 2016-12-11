c313bea450095e535cc9365e8d6b629a70cfa054
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2016-12-11 19:53:46 +0000

    eayslogging++: Fix bad memory access before opening any files

--- external/easylogging++/easylogging++.h
+++ external/easylogging++/easylogging++.h
@@ -3082,6 +3082,7 @@ namespace base {
                 base::utils::Str::trim(boolStr);
                 return (boolStr == "TRUE" || boolStr == "true" || boolStr == "1");
             };
+            setValue(Level::Global, base::FileStreamPtr(NULL), &m_fileStreamMap);
             std::vector<Configuration*> withFileSizeLimit;
             for (Configurations::const_iterator it = configurations->begin(); it != configurations->end(); ++it) {
                 Configuration* conf = *it;
