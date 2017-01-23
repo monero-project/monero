98357db400b803f8518f63095260db3e235d59a4
NanoAkron <nanoakron@users.noreply.github.com>
2017-01-23 01:03:04 +0000

    Update log messages to reflect fact we no longer need to type `exit` to save database before quitting

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -1020,12 +1020,11 @@ namespace cryptonote
       MGINFO_YELLOW(ENDL << "**********************************************************************" << ENDL
         << "The daemon will start synchronizing with the network. It may take up to several hours." << ENDL
         << ENDL
-        << "You can set the level of process detailization* through \"set_log <level|categories>\" command*, where <level> is between 0 (no details) and 4 (very verbose), or custom category based levels (eg, *:WARNING)" << ENDL
+        << "You can set the level of process detailization* through \"set_log <level|categories>\" command*," << ENDL
+        << "where <level> is between 0 (no details) and 4 (very verbose), or custom category based levels (eg, *:WARNING)" << ENDL
         << ENDL
-        << "Use \"help\" command to see the list of available commands." << ENDL
-        << ENDL
-        << "Note: in case you need to interrupt the process, use \"exit\" command. Otherwise, the current progress won't be saved." << ENDL
-        << "**********************************************************************");
+        << "Use the \"help\" command to see the list of available commands." << ENDL
+        << "**********************************************************************" << ENDL);
       m_starter_message_showed = true;
     }
 
