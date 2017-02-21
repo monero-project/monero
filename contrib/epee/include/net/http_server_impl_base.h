dcf684c704471751e670da5232616594649f82a8
anonimal <anonimal@i2pmail.org>
2017-02-21 18:30:43 +0000

    http_server_impl_base: set bind message to MGINFO

--- contrib/epee/include/net/http_server_impl_base.h
+++ contrib/epee/include/net/http_server_impl_base.h
@@ -67,7 +67,7 @@ namespace epee
 
       m_net_server.get_config_object().m_user = std::move(user);
 
-      LOG_PRINT_L0("Binding on " << bind_ip << ":" << bind_port);
+      MGINFO("Binding on " << bind_ip << ":" << bind_port);
       bool res = m_net_server.init_server(bind_port, bind_ip);
       if(!res)
       {
