db1c7d80b18e844206b8205a2eb60fbf60824662
Jaquee <jaquee.monero@gmail.com>
2017-03-02 14:09:04 +0100

    wallet api: add missing mining options

--- src/wallet/wallet2_api.h
+++ src/wallet/wallet2_api.h
@@ -696,7 +696,7 @@ struct WalletManager
     virtual bool isMining() const = 0;
 
     //! starts mining with the set number of threads
-    virtual bool startMining(const std::string &address, uint32_t threads = 1) = 0;
+    virtual bool startMining(const std::string &address, uint32_t threads = 1, bool background_mining = false, bool ignore_battery = true) = 0;
 
     //! stops mining
     virtual bool stopMining() = 0;
