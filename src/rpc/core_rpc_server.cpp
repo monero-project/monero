b553c282fbe733ac35802845c0dfcfb316e2818d
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-03-04 18:45:33 +0000

    rpc: fix BUILD_TAG mispelling (BUILDTAG)
    This ensures a manual or RPC update tries the right build tag,
    rather than source, which is currently not setup

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -1491,8 +1491,8 @@ namespace cryptonote
   bool core_rpc_server::on_update(const COMMAND_RPC_UPDATE::request& req, COMMAND_RPC_UPDATE::response& res)
   {
     static const char software[] = "monero";
-#ifdef BUILDTAG
-    static const char buildtag[] = BOOST_PP_STRINGIZE(BUILDTAG);
+#ifdef BUILD_TAG
+    static const char buildtag[] = BOOST_PP_STRINGIZE(BUILD_TAG);
 #else
     static const char buildtag[] = "source";
 #endif
