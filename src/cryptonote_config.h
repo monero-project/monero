0e3082c3af99006bce4056ead7e4f08ba57cd49a
Hubert <hubert.maslin@gmail.com>
2017-03-17 22:00:00 +0100

    example of changes to emission schedule

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -52,7 +52,10 @@
 // MONEY_SUPPLY - total number coins to be generated
 #define MONEY_SUPPLY                                    ((uint64_t)(-1))
 #define EMISSION_SPEED_FACTOR_PER_MINUTE                (20)
-#define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)300000000000) // 3 * pow(10, 11)
+
+//fork: divide final subsidy threshold by 30 to ensure emission until 2040
+//#define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)300000000000) // 3 * pow(10, 11)
+#define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)10000000000) // 3 * pow(10, 11)
 
 #define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 100
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    60000 //size of block (bytes) after which reward for block calculated using block size
