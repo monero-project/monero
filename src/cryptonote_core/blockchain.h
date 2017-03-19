50cd179a609b036127fc0391493be2f893159c94
Lee Clagett <code@leeclagett.com>
2017-03-18 22:05:14 -0400

    Removed boost/asio.hpp include from epee/string_tools.h

--- src/cryptonote_core/blockchain.h
+++ src/cryptonote_core/blockchain.h
@@ -29,6 +29,7 @@
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #pragma once
+#include <boost/asio/io_service.hpp>
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/version.hpp>
 #include <boost/serialization/list.hpp>
