31616af33d3c2ee77ceb31eff291755fffba4b94
assylias <yann@assylias.com>
2017-03-23 16:46:31 +0000

    Fixes #1850 - Add French mnemonics wordlist

--- src/mnemonics/electrum-words.cpp
+++ src/mnemonics/electrum-words.cpp
@@ -52,6 +52,7 @@
 #include <boost/algorithm/string/join.hpp>
 
 #include "english.h"
+#include "french.h"
 #include "italian.h"
 #include "german.h"
 #include "spanish.h"
@@ -83,6 +84,7 @@ namespace
     // If there's a new language added, add an instance of it here.
     std::vector<Language::Base*> language_instances({
       Language::Singleton<Language::English>::instance(),
+      Language::Singleton<Language::French>::instance(),
       Language::Singleton<Language::Spanish>::instance(),
       Language::Singleton<Language::German>::instance(),
       Language::Singleton<Language::Italian>::instance(),
@@ -312,6 +314,10 @@ namespace crypto
       {
         language = Language::Singleton<Language::English>::instance();
       }
+      else if (language_name == "French")
+      {
+        language = Language::Singleton<Language::French>::instance();
+      }
       else if (language_name == "Spanish")
       {
         language = Language::Singleton<Language::Spanish>::instance();
@@ -382,6 +388,7 @@ namespace crypto
     {
       std::vector<Language::Base*> language_instances({
         Language::Singleton<Language::English>::instance(),
+        Language::Singleton<Language::French>::instance(),
         Language::Singleton<Language::Spanish>::instance(),
         Language::Singleton<Language::German>::instance(),
         Language::Singleton<Language::Italian>::instance(),
