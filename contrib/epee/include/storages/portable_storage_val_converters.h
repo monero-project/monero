bec501e5ad9b5d7b42220ffbd9181a81ebcb9a22
Jaquee <jaquee.monero@gmail.com>
2017-07-22 10:10:21 +0200

    Serializer: string to integer conversion for MyMonero compatibility
    mymonero timestamp conversion

--- contrib/epee/include/storages/portable_storage_val_converters.h
+++ contrib/epee/include/storages/portable_storage_val_converters.h
@@ -28,6 +28,8 @@
 
 #pragma once
 
+#include <regex>
+
 #include "misc_language.h"
 #include "portable_storage_base.h"
 #include "warnings.h"
@@ -128,6 +130,31 @@ POP_WARNINGS
       static void convert(const from_type& from, to_type& to)
       {
         ASSERT_AND_THROW_WRONG_CONVERSION();
+      }
+    };
+
+    // For MyMonero/OpenMonero backend compatibility
+    // MyMonero backend sends amount, fees and timestamp values as strings.
+    // Until MM backend is updated, this is needed for compatibility between OpenMonero and MyMonero. 
+    template<>
+    struct convert_to_integral<std::string, uint64_t, false>
+    {
+      static void convert(const std::string& from, uint64_t& to)
+      {
+        MTRACE("Converting std::string to uint64_t. Source: " << from);
+        // String only contains digits
+        if(std::all_of(from.begin(), from.end(), ::isdigit))
+          to = boost::lexical_cast<uint64_t>(from);
+        // MyMonero ISO 8061 timestamp (2017-05-06T16:27:06Z)
+        else if (std::regex_match (from, std::regex("\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\dZ")))
+        {
+          // Convert to unix timestamp
+          std::tm tm = {};
+          std::istringstream ss(from);
+          if (ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S"))
+            to = std::mktime(&tm);
+        } else
+          ASSERT_AND_THROW_WRONG_CONVERSION();
       }
     };
 
