525975acc49877e2974f320f17baeaaa7347f341
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-07-27 20:36:28 +1000

    wallet-cli: Minor improvement to help output

--- src/wallet/wallet_args.cpp
+++ src/wallet/wallet_args.cpp
@@ -149,8 +149,10 @@ namespace wallet_args
 
     if (command_line::get_arg(vm, command_line::arg_help))
     {
-      tools::msg_writer() << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")";
-      tools::msg_writer() << wallet_args::tr("Usage:") << ' ' << usage;
+      tools::msg_writer() << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL;
+      tools::msg_writer() << wallet_args::tr("This is the command line monero wallet. It needs to connect to a monero\n"
+												"daemon to work correctly.") << ENDL;
+      tools::msg_writer() << wallet_args::tr("Usage:") << ENDL << "  " << usage;
       tools::msg_writer() << desc_all;
       return boost::none;
     }
