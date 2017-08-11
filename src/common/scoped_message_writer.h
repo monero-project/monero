7ed5ab47ea05900250e72bafb11d0e0fc3d180b9
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-08-11 13:38:18 +0100

    scoped_message_writer: pause readline to match simplewallet

--- src/common/scoped_message_writer.h
+++ src/common/scoped_message_writer.h
@@ -31,6 +31,14 @@
 #include "misc_log_ex.h"
 #include <iostream>
 
+#ifdef HAVE_READLINE
+  #include "readline_buffer.h"
+  #define PAUSE_READLINE() \
+    rdln::suspend_readline pause_readline; 
+#else
+  #define PAUSE_READLINE()
+#endif
+
 namespace tools
 {
 
@@ -99,6 +107,7 @@ public:
       }
       else
       {
+        PAUSE_READLINE();
         set_console_color(m_color, m_bright);
         std::cout << m_oss.str();
         epee::reset_console_color();
