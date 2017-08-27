adce8ae466cd86167f55b757dcf1188850f1cb27
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-08-27 21:04:56 +0100

    simplewallet: new "fee" command to display fee information
    including expected transaction backlog at different priorities

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -175,6 +175,7 @@ namespace cryptonote
     bool show_transfer(const std::vector<std::string> &args);
     bool change_password(const std::vector<std::string>& args);
     bool payment_id(const std::vector<std::string> &args);
+    bool print_fee_info(const std::vector<std::string> &args);
 
     uint64_t get_daemon_blockchain_height(std::string& err);
     bool try_connect_to_daemon(bool silent = false, uint32_t* version = nullptr);
