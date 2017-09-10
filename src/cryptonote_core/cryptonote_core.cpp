3deef4018eb4b851916bea300f1d52825db12469
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-09-10 15:33:13 +0100

    core: guard against exceptions in tx verification worker threads

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -597,7 +597,15 @@ namespace cryptonote
       std::list<blobdata>::const_iterator it = tx_blobs.begin();
       for (size_t i = 0; i < tx_blobs.size(); i++, ++it) {
         region.run([&, i, it] {
-          results[i].res = handle_incoming_tx_pre(*it, tvc[i], results[i].tx, results[i].hash, results[i].prefix_hash, keeped_by_block, relayed, do_not_relay);
+          try
+          {
+            results[i].res = handle_incoming_tx_pre(*it, tvc[i], results[i].tx, results[i].hash, results[i].prefix_hash, keeped_by_block, relayed, do_not_relay);
+          }
+          catch (const std::exception &e)
+          {
+            MERROR_VER("Exception in handle_incoming_tx_pre: " << e.what());
+            results[i].res = false;
+          }
         });
       }
     });
@@ -617,7 +625,15 @@ namespace cryptonote
         else
         {
           region.run([&, i, it] {
-            results[i].res = handle_incoming_tx_post(*it, tvc[i], results[i].tx, results[i].hash, results[i].prefix_hash, keeped_by_block, relayed, do_not_relay);
+            try
+            {
+              results[i].res = handle_incoming_tx_post(*it, tvc[i], results[i].tx, results[i].hash, results[i].prefix_hash, keeped_by_block, relayed, do_not_relay);
+            }
+            catch (const std::exception &e)
+            {
+              MERROR_VER("Exception in handle_incoming_tx_post: " << e.what());
+              results[i].res = false;
+            }
           });
         }
       }
