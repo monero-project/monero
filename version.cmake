a8830f8fcd2923d033ab5b3f2cc1465c0c1f4c48
stoffu <stoffu@protonmail.ch>
2017-09-13 12:01:58 +0900

    build: auto update version info without manually deleting version.h

--- version.cmake
+++ version.cmake
@@ -1,14 +1,15 @@
-function (write_static_version_header hash)
+function (write_static_version_inl hash)
   set(VERSIONTAG "${hash}")
-  configure_file("src/version.h.in" "version/version.h")
+  configure_file("src/version.inl.in" "version/version.inl")
   add_custom_target(version ALL)
 endfunction ()
 
 file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/version")
+file(REMOVE "${CMAKE_BINARY_DIR}/version/version.inl")
 find_package(Git QUIET)
 if ("$Format:$" STREQUAL "")
   # We're in a tarball; use hard-coded variables.
-  write_static_version_header("release")
+  write_static_version_inl("release")
 elseif (GIT_FOUND OR Git_FOUND)
   message(STATUS "Found Git: ${GIT_EXECUTABLE}")
   set(extra_output)
@@ -18,16 +19,16 @@ elseif (GIT_FOUND OR Git_FOUND)
     set(extra_output "${CMAKE_SOURCE_DIR}/.force-git-version-check")
   endif ()
   add_custom_command(
-    OUTPUT            "${CMAKE_BINARY_DIR}/version/version.h"
+    OUTPUT            "${CMAKE_BINARY_DIR}/version/version.inl"
                       ${extra_output}
     COMMAND           "${CMAKE_COMMAND}"
                       "-D" "GIT=${GIT_EXECUTABLE}"
-                      "-D" "TO=${CMAKE_BINARY_DIR}/version/version.h"
+                      "-D" "TO=${CMAKE_BINARY_DIR}/version/version.inl"
                       "-P" "src/version.cmake"
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
   add_custom_target(version ALL
-    DEPENDS "${CMAKE_BINARY_DIR}/version/version.h")
+    DEPENDS "${CMAKE_BINARY_DIR}/version/version.inl")
 else()
   message(STATUS "WARNING: Git was not found!")
-  write_static_version_header("unknown")
+  write_static_version_inl("unknown")
 endif ()
