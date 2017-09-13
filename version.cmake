2db9c7bf5a988e602a74b08248eacaeaab0f143b
Howard Chu <hyc@symas.com>
2017-09-13 19:38:32 +0100

    Only use 2 files not 3

--- version.cmake
+++ version.cmake
@@ -1,11 +1,10 @@
 function (write_static_version_inl hash)
   set(VERSIONTAG "${hash}")
-  configure_file("src/version.inl.in" "version/version.inl")
-  add_custom_target(version ALL)
+  configure_file("src/version.cpp.in" "version/version.cpp")
 endfunction ()
 
 file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/version")
-file(REMOVE "${CMAKE_BINARY_DIR}/version/version.inl")
+file(REMOVE "${CMAKE_BINARY_DIR}/version/version.cpp")
 find_package(Git QUIET)
 if ("$Format:$" STREQUAL "")
   # We're in a tarball; use hard-coded variables.
@@ -19,16 +18,16 @@ elseif (GIT_FOUND OR Git_FOUND)
     set(extra_output "${CMAKE_SOURCE_DIR}/.force-git-version-check")
   endif ()
   add_custom_command(
-    OUTPUT            "${CMAKE_BINARY_DIR}/version/version.inl"
+    OUTPUT            "${CMAKE_BINARY_DIR}/version/version.cpp"
                       ${extra_output}
     COMMAND           "${CMAKE_COMMAND}"
                       "-D" "GIT=${GIT_EXECUTABLE}"
-                      "-D" "TO=${CMAKE_BINARY_DIR}/version/version.inl"
+                      "-D" "TO=${CMAKE_BINARY_DIR}/version/version.cpp"
                       "-P" "src/version.cmake"
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
-  add_custom_target(version ALL
-    DEPENDS "${CMAKE_BINARY_DIR}/version/version.inl")
 else()
   message(STATUS "WARNING: Git was not found!")
   write_static_version_inl("unknown")
 endif ()
+add_custom_target(version ALL
+  DEPENDS "${CMAKE_BINARY_DIR}/version/version.cpp")
