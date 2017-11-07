fabb7d50494361432c372070720c3b0767bb3582
Maxime THIEBAUT <m.thiebaut@student.helmo.be>
2017-11-07 19:15:16 +0100

    Extended condition

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -1472,7 +1472,7 @@ bool simple_wallet::init(const boost::program_options::variables_map& vm)
     if (m_restoring)
     {
       uint64_t approximate_blockchain_height = simple_wallet::get_approximate_blockchain_height();
-      if (m_restore_height > approximate_blockchain_height){
+      if (m_restore_height >= approximate_blockchain_height){
         message_writer() << tr("The restore height ") << m_restore_height << tr(" seems too high as the approximate blockchain height is ") << approximate_blockchain_height;
         std::string confirm = command_line::input_line(tr("Proceed anyway?  (Y/Yes/N/No): "));
         if (std::cin.eof() || !command_line::is_yes(confirm))
