95aa0bf79b968d7ea04a51561a176ae1f5a460bd
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-12-09 23:04:49 +0000

    add load_from_binary/load_from_json fuzzers

--- contrib/fuzz_testing/fuzz.sh
+++ contrib/fuzz_testing/fuzz.sh
@@ -10,12 +10,12 @@ fi
 type="$1"
 if test -z "$type"
 then
-  echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction"
+  echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json"
   exit 1
 fi
 case "$type" in
-  block|transaction|signature|cold-outputs|cold-transaction) ;;
-  *) echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction"; exit 1 ;;
+  block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json) ;;
+  *) echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json"; exit 1 ;;
 esac
 
 afl-fuzz -i tests/data/fuzz/$type -m 150 -t 250 -o fuzz-out/$type build/fuzz/tests/fuzz/${type}_fuzz_tests
