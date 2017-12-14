a1d44f2746955abbf170f407f6613e3bddae3225
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-12-14 13:33:06 +0000

    updates: use https for updates

--- src/common/updates.cpp
+++ src/common/updates.cpp
@@ -98,7 +98,7 @@ namespace tools
 
   std::string get_update_url(const std::string &software, const std::string &subdir, const std::string &buildtag, const std::string &version, bool user)
   {
-    const char *base = user ? "https://downloads.getmonero.org/" : "http://updates.getmonero.org/";
+    const char *base = user ? "https://downloads.getmonero.org/" : "https://updates.getmonero.org/";
 #ifdef _WIN32
     static const char *extension = strncmp(buildtag.c_str(), "install-", 8) ? ".zip" : ".exe";
 #else
