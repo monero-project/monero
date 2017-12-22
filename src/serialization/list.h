2d17feb060275083424d3b5978282b22a5dc88ac
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-12-22 19:47:12 +0000

    factor STL container serialization

--- src/serialization/list.h
+++ src/serialization/list.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2015, The Monero Project
+// Copyright (c) 2014-2017, The Monero Project
 // 
 // All rights reserved.
 // 
@@ -30,71 +30,29 @@
 
 #pragma once
 
-#include "serialization.h"
+#include <list>
+
+template <template <bool> class Archive, class T>
+bool do_serialize(Archive<false> &ar, std::list<T> &v);
+template <template <bool> class Archive, class T>
+bool do_serialize(Archive<true> &ar, std::list<T> &v);
 
 namespace serialization
 {
   namespace detail
   {
-    template <typename Archive, class T>
-    bool serialize_list_element(Archive& ar, T& e)
+    template <typename T>
+    void do_add(std::list<T> &c, T &&e)
     {
-      return ::do_serialize(ar, e);
-    }
-
-    template <typename Archive>
-    bool serialize_list_element(Archive& ar, uint64_t& e)
-    {
-      ar.serialize_varint(e);
-      return true;
+      c.emplace_back(std::move(e));
     }
   }
 }
 
-template <template <bool> class Archive, class T>
-bool do_serialize(Archive<false> &ar, std::list<T> &l)
-{
-  size_t cnt;
-  ar.begin_array(cnt);
-  if (!ar.stream().good())
-    return false;
-  l.clear();
-
-  // very basic sanity check
-  if (ar.remaining_bytes() < cnt) {
-    ar.stream().setstate(std::ios::failbit);
-    return false;
-  }
-
-  for (size_t i = 0; i < cnt; i++) {
-    if (i > 0)
-      ar.delimit_array();
-    l.push_back(T());
-    T &t = l.back();
-    if (!::serialization::detail::serialize_list_element(ar, t))
-      return false;
-    if (!ar.stream().good())
-      return false;
-  }
-  ar.end_array();
-  return true;
-}
+#include "serialization.h"
 
 template <template <bool> class Archive, class T>
-bool do_serialize(Archive<true> &ar, std::list<T> &l)
-{
-  size_t cnt = l.size();
-  ar.begin_array(cnt);
-  for (typename std::list<T>::iterator i = l.begin(); i != l.end(); ++i) {
-    if (!ar.stream().good())
-      return false;
-    if (i != l.begin())
-      ar.delimit_array();
-    if(!::serialization::detail::serialize_list_element(ar, *i))
-      return false;
-    if (!ar.stream().good())
-      return false;
-  }
-  ar.end_array();
-  return true;
-}
+bool do_serialize(Archive<false> &ar, std::list<T> &v) { return do_serialize_container(ar, v); }
+template <template <bool> class Archive, class T>
+bool do_serialize(Archive<true> &ar, std::list<T> &v) { return do_serialize_container(ar, v); }
+
