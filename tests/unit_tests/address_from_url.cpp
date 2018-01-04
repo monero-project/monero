9ffa97fe982fa14cbceaf2a88049f30237a96eb9
Maxithi <34792056+Maxithi@users.noreply.github.com>
2018-01-04 15:33:20 +0100

    Factor the monero donation address
    Signed-off-by: Maxithi <34792056+Maxithi@users.noreply.github.com>

--- tests/unit_tests/address_from_url.cpp
+++ tests/unit_tests/address_from_url.cpp
@@ -32,6 +32,7 @@
 
 #include "wallet/wallet2.h"
 #include "common/dns_utils.h"
+#include "simplewallet/simplewallet.h"
 #include <string>
 
 TEST(AddressFromTXT, Success)
@@ -83,7 +84,7 @@ TEST(AddressFromTXT, Failure)
 
 TEST(AddressFromURL, Success)
 {
-  const std::string addr = "44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A";
+  const std::string addr = MONERO_DONATION_ADDR;
   
   bool dnssec_result = false;
 
