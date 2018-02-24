a332aa35569cc2d7ae107fe9e4ff8a81307fc655
stoffu <stoffu@protonmail.ch>
2018-02-24 18:38:51 +0900

    electrum-words: in create_checksum_index, divide by word list length instead of const seed_length=24

--- src/mnemonics/electrum-words.cpp
+++ src/mnemonics/electrum-words.cpp
@@ -194,7 +194,7 @@ namespace
     }
     boost::crc_32_type result;
     result.process_bytes(trimmed_words.data(), trimmed_words.length());
-    return result.checksum() % crypto::ElectrumWords::seed_length;
+    return result.checksum() % word_list.size();
   }
 
   /*!
