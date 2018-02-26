92c8bf36386d3c377c700895db81f948d79e2f4c
Thomas Winget <tewinget@gmail.com>
2018-02-26 12:47:31 -0500

    loki premine reward

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -87,6 +87,14 @@ namespace cryptonote {
   }
   //-----------------------------------------------------------------------------------------------
   bool get_block_reward(size_t median_size, size_t current_block_size, uint64_t already_generated_coins, uint64_t &reward, uint8_t version) {
+
+    //premine reward
+    if (already_generated_coins == 0)
+    {
+      reward = 40000000000000000;
+      return true;
+    }
+
     static_assert(DIFFICULTY_TARGET_V2%60==0&&DIFFICULTY_TARGET_V1%60==0,"difficulty targets must be a multiple of 60");
     const int target = version < 2 ? DIFFICULTY_TARGET_V1 : DIFFICULTY_TARGET_V2;
     const int target_minutes = target / 60;
