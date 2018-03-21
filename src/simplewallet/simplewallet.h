462579d689082a943ee0f9e6de21da254f013974
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-03-21 19:00:27 +0000

    simplewallet: add --untrusted-daemon option /monero#3477

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -229,6 +229,7 @@ namespace cryptonote
     bool print_ring_members(const std::vector<tools::wallet2::pending_tx>& ptx_vector, std::ostream& ostr);
     std::string get_prompt() const;
     bool print_seed(bool encrypted);
+    bool is_daemon_trusted() const { return *m_trusted_daemon; }
 
     /*!
      * \brief Prints the seed with a nice message
@@ -331,7 +332,7 @@ namespace cryptonote
     bool m_restore_deterministic_wallet;  // recover flag
     bool m_restore_multisig_wallet;  // recover flag
     bool m_non_deterministic;  // old 2-random generation
-    bool m_trusted_daemon;
+    boost::optional<bool> m_trusted_daemon;
     bool m_allow_mismatched_daemon_version;
     bool m_restoring;           // are we restoring, by whatever method?
     uint64_t m_restore_height;  // optional
