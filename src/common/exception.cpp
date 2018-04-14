0838535e74f057ad2140a22b17464a3bcc22ea1a
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-04-14 10:49:17 +0100

    optional boost stacktrace to get stack traces on mac and windows too
    also straighten out stacktrace-on-exception for tests
    
    Thanks to iDunk for lots of testing to get this to work on Windows

--- src/common/exception.cpp
+++ src/common/exception.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2020, The Monero Project
+// Copyright (c) 2016-2018, The Monero Project
 //
 // All rights reserved.
 //
@@ -33,10 +33,15 @@
 #endif
 #include "easylogging++/easylogging++.h"
 
+#include <stdexcept>
 #ifdef USE_LIBUNWIND_STACKTRACE
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
+#include <cxxabi.h>
 #include <iomanip>
+#endif
+#ifndef STATICLIB
+#include <dlfcn.h>
 #endif
 #include <boost/algorithm/string.hpp>
 #include "common/stack_trace.h"
@@ -56,81 +61,49 @@
     } \
   } while(0)
 
-namespace
-{
-  std::string stack_trace_log;
-}
+#ifdef STACK_TRACE_ON_EXCEPTIONS
 
-namespace tools
-{
+// from https://stackoverflow.com/questions/11665829/how-can-i-print-stack-trace-for-caught-exceptions-in-c-code-injection-in-c
 
-void set_stack_trace_log(const std::string &log)
-{
-  stack_trace_log = log;
-}
+// The decl of __cxa_throw in /usr/include/.../cxxabi.h uses
+// 'std::type_info *', but GCC's built-in protype uses 'void *'.
+#ifdef __clang__
+#define CXA_THROW_INFO_T std::type_info
+#else // !__clang__
+#define CXA_THROW_INFO_T void
+#endif // !__clang__
 
-void log_stack_trace(const char *msg)
+#ifdef STATICLIB
+#define CXA_THROW __wrap___cxa_throw
+extern "C"
+__attribute__((noreturn))
+void __real___cxa_throw(void *ex, CXA_THROW_INFO_T *info, void (*dest)(void*));
+#else // !STATICLIB
+#define CXA_THROW __cxa_throw
+extern "C"
+typedef
+#ifdef __clang__ // only clang, not GCC, lets apply the attr in typedef
+__attribute__((noreturn))
+#endif // __clang__
+void (cxa_throw_t)(void *ex, CXA_THROW_INFO_T *info, void (*dest)(void*));
+#endif // !STATICLIB
+
+extern "C"
+__attribute__((noreturn))
+void CXA_THROW(void *ex, CXA_THROW_INFO_T *info, void (*dest)(void*))
 {
-#ifdef USE_LIBUNWIND_STACKTRACE
-  unw_context_t ctx;
-  unw_cursor_t cur;
-  unw_word_t ip, off;
-  unsigned level;
-  char sym[512], *dsym;
   int status;
-  const char *log = stack_trace_log.empty() ? NULL : stack_trace_log.c_str();
-#endif
+  char *dsym = abi::__cxa_demangle(((const std::type_info*)info)->name(), NULL, NULL, &status);
+  tools::log_stack_trace((std::string("Exception: ")+((!status && dsym) ? dsym : (const char*)info)).c_str());
+  free(dsym);
 
-  if (msg)
-    ST_LOG(msg);
-  ST_LOG("Unwound call stack:");
-
-#ifdef USE_LIBUNWIND_STACKTRACE
-  if (unw_getcontext(&ctx) < 0) {
-    ST_LOG("Failed to create unwind context");
-    return;
-  }
-  if (unw_init_local(&cur, &ctx) < 0) {
-    ST_LOG("Failed to find the first unwind frame");
-    return;
-  }
-  for (level = 1; level < 999; ++level) { // 999 for safety
-    int ret = unw_step(&cur);
-    if (ret < 0) {
-      ST_LOG("Failed to find the next frame");
-      return;
-    }
-    if (ret == 0)
-      break;
-    if (unw_get_reg(&cur, UNW_REG_IP, &ip) < 0) {
-      ST_LOG("  " << std::setw(4) << level);
-      continue;
-    }
-    if (unw_get_proc_name(&cur, sym, sizeof(sym), &off) < 0) {
-      ST_LOG("  " << std::setw(4) << level << std::setbase(16) << std::setw(20) << "0x" << ip);
-      continue;
-    }
-    dsym = abi::__cxa_demangle(sym, NULL, NULL, &status);
-    ST_LOG("  " << std::setw(4) << level << std::setbase(16) << std::setw(20) << "0x" << ip << " " << (!status && dsym ? dsym : sym) << " + " << "0x" << off);
-    free(dsym);
-  }
-#elif defined USE_BOOST_STACKTRACE
-  std::stringstream ss;
-  ss << boost::stacktrace::stacktrace();
-  std::vector<std::string> lines;
-  std::string s = ss.str();
-  boost::split(lines, s, boost::is_any_of("\n"));
-  for (const auto &line: lines)
-    ST_LOG(line);
-#elif defined USE_EASYLOGGING_STACKTRACE
-  std::stringstream ss;
-  ss << el::base::debug::StackTrace();
-  std::vector<std::string> lines;
-  std::string s = ss.str();
-  boost::split(lines, s, boost::is_any_of("\n"));
-  for (const auto &line: lines)
-    ST_LOG(line);
-#endif
+#ifndef STATICLIB
+#ifndef __clang__ // for GCC the attr can't be applied in typedef like for clang
+  __attribute__((noreturn))
+#endif // !__clang__
+   cxa_throw_t *__real___cxa_throw = (cxa_throw_t*)dlsym(RTLD_NEXT, "__cxa_throw");
+#endif // !STATICLIB
+  __real___cxa_throw(ex, info, dest);
 }
 
-}  // namespace tools
+#endif
