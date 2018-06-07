65b4a91669998c14276d83afcfa5392c34d04392
Seyed Ali Tabatabaee <The_CodeBreakeR@Seyeds-MacBook-Pro.local>
2018-06-07 11:21:48 +0300

    ult

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 // fsdafdsfsdfsdafas
 extern const char* const MONERO_VERSION_TAG;
-extern const char* const MONERO_VERSION;
 extern const char* const MONERO_RELEASE_NAME;
 extern const char* const MONERO_VERSION_FULL;
+//fsdafasdf
