55c7fb87a99cbd89fe2d98de7bde9854ee12b161
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-06-07 12:43:10 +0100

    epee: adaptive connection timeout system
    a connection's timeout is halved for every extra connection
    from the same host.
    
    Also keep track of when we don't need to use a connection
    anymore, so we can close it and free the resource for another
    connection.
    
    Also use the longer timeout for non routable local addresses.

--- tests/fuzz/levin.cpp
+++ tests/fuzz/levin.cpp
@@ -158,6 +158,7 @@ namespace
     }
 
     virtual bool close()                              { return true; }
+    virtual bool send_done()                          { return true; }
     virtual bool call_run_once_service_io()           { return true; }
     virtual bool request_callback()                   { return true; }
     virtual boost::asio::io_service& get_io_service() { return m_io_service; }
