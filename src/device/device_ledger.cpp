03096150c626066bedf451846e6dbedd995df1aa
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-06-09 20:49:05 +0100

    device_ledger: fix bad memory access on connect error

--- src/device/device_ledger.cpp
+++ src/device/device_ledger.cpp
@@ -396,7 +396,7 @@ namespace hw {
         }
       }
 
-      if (mszReaders) {
+      if (rv == SCARD_S_SUCCESS && mszReaders) {
         #ifdef SCARD_AUTOALLOCATE
         SCardFreeMemory(this->hContext, mszReaders);
         #else
