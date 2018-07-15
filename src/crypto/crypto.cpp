1f8f46360e5beace13242105333b6d439597c6d3
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-07-15 12:59:58 +0100

    crypto: move null_pkey/null_skey to the cpp file /monero#4137

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -70,6 +70,9 @@ namespace crypto {
 #include "random.h"
   }
 
+  const crypto::public_key null_pkey = boost::value_initialized<crypto::public_key>();
+  const crypto::secret_key null_skey = boost::value_initialized<crypto::secret_key>();
+
   static inline unsigned char *operator &(ec_point &point) {
     return &reinterpret_cast<unsigned char &>(point);
   }
