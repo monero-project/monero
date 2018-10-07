24e519b9b6076334a652cf4b73cc0b5dc1a45cd6
Riccardo Spagni <ric@spagni.net>
2018-10-07 19:51:09 +0200

    Merge pull request #4510
    21a624af Consolidate HID depends makefiles into single recipe (TheCharlatan)

--- contrib/depends/packages/sodium.mk
+++ contrib/depends/packages/sodium.mk
@@ -5,7 +5,7 @@ $(package)_file_name=libsodium-$($(package)_version).tar.gz
 $(package)_sha256_hash=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe9e178f4
 
 define $(package)_set_vars
-$(package)_config_opts=--enable-static
+$(package)_config_opts=--enable-static --disable-shared
 $(package)_config_opts+=--prefix=$(host_prefix)
 endef
 
