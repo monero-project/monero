e629bc5ac92666a10b965ac31812427941af15b6
TheCharlatan <seb.kung@gmail.com>
2018-10-09 11:12:32 +0200

    Change gitian.sigs repo from bitcoin-core to monero-project remote host

--- contrib/gitian/gitian-build.py
+++ contrib/gitian/gitian-build.py
@@ -23,7 +23,7 @@ def setup():
         programs += ['lxc', 'debootstrap']
     subprocess.check_call(['sudo', 'apt-get', 'install', '-qq'] + programs)
     if not os.path.isdir('gitian.sigs'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/bitcoin-core/gitian.sigs.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/monero-project/gitian.sigs.git'])
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('monero'):
