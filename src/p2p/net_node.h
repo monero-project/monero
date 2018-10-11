d605d625b225da18fe1cb1ca1df6043f9f6fcd3b
calidion <calidion@gmail.com>
2018-10-12 01:39:58 +0800

    separate data from code

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -126,12 +126,6 @@ namespace nodetool
     virtual bool unblock_host(const epee::net_utils::network_address &address);
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
-    const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.moneroseeds.se"
-    , "seeds.moneroseeds.ae.org"
-    , "seeds.moneroseeds.ch"
-    , "seeds.moneroseeds.li"
-    };
 
     bool islimitup=false;
     bool islimitdown=false;
