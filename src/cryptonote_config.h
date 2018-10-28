9c1566a81e0dce84369b39c50f159e9dcb5390c4
Jameson Lopp <jameson.lopp@gmail.com>
2018-10-28 11:28:21 -0400

    comment spelling fix

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -104,7 +104,7 @@
 #define P2P_LOCAL_GRAY_PEERLIST_LIMIT                   5000
 
 #define P2P_DEFAULT_CONNECTIONS_COUNT                   8
-#define P2P_DEFAULT_HANDSHAKE_INTERVAL                  60           //secondes
+#define P2P_DEFAULT_HANDSHAKE_INTERVAL                  60           //seconds
 #define P2P_DEFAULT_PACKET_MAX_SIZE                     50000000     //50000000 bytes maximum packet size
 #define P2P_DEFAULT_PEERS_IN_HANDSHAKE                  250
 #define P2P_DEFAULT_CONNECTION_TIMEOUT                  5000       //5 seconds
