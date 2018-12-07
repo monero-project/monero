b4433abc6452de45ec6f2b97971aeb0d5ee2bd7c
TheCharlatan <seb.kung@gmail.com>
2018-12-08 00:16:27 +0100

    Optimize the depends builds for faster compilation
    This includes more fine grained configure options and skipping the
    openssl and zlib dependencies when compiling qt. The zlib and libevent
    packages are removed.

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl libevent zeromq cppzmq zlib expat ldns cppzmq readline libiconv qt hidapi protobuf libusb
+packages:=boost openssl zeromq cppzmq expat ldns cppzmq readline libiconv qt hidapi protobuf libusb
 native_packages := native_ccache native_protobuf
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
@@ -17,6 +17,5 @@ endif
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
-packages += readline
 endif
 
