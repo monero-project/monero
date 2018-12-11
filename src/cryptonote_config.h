806dc8fbe96846a02454026d92dbe2ebf2344abb
hypeboard <37452765+hypeboard@users.noreply.github.com>
2018-12-11 14:12:59 -0600

    rebrand

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -126,7 +126,7 @@
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME                         "bitmonero"
+#define CRYPTONOTE_NAME                         "venue"
 #define CRYPTONOTE_POOLDATA_FILENAME            "poolstate.bin"
 #define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
 #define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
