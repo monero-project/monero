6aaa092d1753f171591c65c5dd61f2935fac9e61
doy-lee <doylet@protonmail.com>
2019-01-30 15:21:10 +1100

    Update new default log category

--- src/common/notify.cpp
+++ src/common/notify.cpp
@@ -33,8 +33,8 @@
 #include "spawn.h"
 #include "notify.h"
 
-#undef MONERO_DEFAULT_LOG_CATEGORY
-#define MONERO_DEFAULT_LOG_CATEGORY "notify"
+#undef LOKI_DEFAULT_LOG_CATEGORY
+#define LOKI_DEFAULT_LOG_CATEGORY "notify"
 
 namespace tools
 {
--- src/common/spawn.cpp
+++ src/common/spawn.cpp
@@ -42,8 +42,8 @@
 #include "util.h"
 #include "spawn.h"
 
-#undef MONERO_DEFAULT_LOG_CATEGORY
-#define MONERO_DEFAULT_LOG_CATEGORY "spawn"
+#undef LOKI_DEFAULT_LOG_CATEGORY
+#define LOKI_DEFAULT_LOG_CATEGORY "spawn"
 
 namespace tools
 {
