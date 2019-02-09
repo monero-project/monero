ed8a26d3e2a43b93603654dabde73bc253f729b0
Unknown <harrisonhesslink24@gmail.com>
2019-02-08 21:28:50 -0800

    approximate_height

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -10229,9 +10229,9 @@ uint64_t wallet2::get_daemon_blockchain_target_height(string &err)
 uint64_t wallet2::get_approximate_blockchain_height() const
 {
   // time of v2 fork
-  const time_t fork_time = m_nettype == TESTNET ? 1448285909 : m_nettype == STAGENET ? 1520937818 : 1458748658;
+  const time_t fork_time = m_nettype == TESTNET ? 25000 : m_nettype == STAGENET ? 25000 : 25000;
   // v2 fork block
-  const uint64_t fork_block = m_nettype == TESTNET ? 624634 : m_nettype == STAGENET ? 32000 : 1009827;
+  const uint64_t fork_block = m_nettype == TESTNET ? 0 : m_nettype == STAGENET ? 0 : 25000;
   // avg seconds per block
   const int seconds_per_block = DIFFICULTY_TARGET_V2;
   // Calculated blockchain height
