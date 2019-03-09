dc0c0c91c52dd7e41e658778beefe43cc528f26f
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-03-09 09:07:42 +0000

    tests: disable wallet SSL init for tests involving wallet2

--- tests/unit_tests/multisig.cpp
+++ tests/unit_tests/multisig.cpp
@@ -71,7 +71,7 @@ static void make_wallet(unsigned int idx, tools::wallet2 &wallet)
 
   try
   {
-    wallet.init("");
+    wallet.init("", boost::none, 0, true, epee::net_utils::ssl_support_t::e_ssl_support_disabled);
     wallet.set_subaddress_lookahead(1, 1);
     wallet.generate("", "", spendkey, true, false);
     ASSERT_TRUE(test_addresses[idx].address == wallet.get_account().get_public_address_str(cryptonote::TESTNET));
