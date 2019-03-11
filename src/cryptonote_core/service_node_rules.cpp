e4e792eb9d565e505515a7110673c63c3a3dca06
Unknown <harrisonhesslink24@gmail.com>
2019-03-10 19:31:20 -0700

    Staking Requirment

--- src/cryptonote_core/service_node_rules.cpp
+++ src/cryptonote_core/service_node_rules.cpp
@@ -17,10 +17,10 @@ namespace service_nodes {
 		if (height < hardfork_height) height = hardfork_height;
 
 		uint64_t height_adjusted = height - hardfork_height;
-		uint64_t base = 1000 * COIN;
+		uint64_t base = 10000 * COIN;
 		uint64_t variable = (3500.0 * COIN) / triton::exp2(height_adjusted / 129600.0);
-		uint64_t linear_up = (uint64_t)(5 * COIN * height / 2592) + 8000 * COIN;
-		uint64_t flat = 1500 * COIN;
+		uint64_t linear_up = (uint64_t)(5 * COIN * height / 480) + 80000 * COIN;
+		uint64_t flat = 15000 * COIN;
 		return std::max(base + variable, height < 175200 ? linear_up : flat);
 	}
 
