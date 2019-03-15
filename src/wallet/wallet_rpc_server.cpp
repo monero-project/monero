4ee156556dfbb27fa460749bf7ae15a9df904ede
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-03-15 18:29:59 +0000

    wallet_rpc_server: fix buffer read overflow in string assignment

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -3331,7 +3331,7 @@ namespace tools
       er.message = "Failed to encode seed";
       return false;
     }
-    res.seed = electrum_words.data();
+    res.seed = std::string(electrum_words.data(), electrum_words.size());
 
     if (!wal)
     {
