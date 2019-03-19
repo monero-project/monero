c854197b6e031794d49eab7d7a7ba688a06aca63
Doyle <doylet@protonmail.com>
2019-03-19 16:33:44 +1100

    Update libwallet api for the GUI wallet

--- src/wallet/api/wallet.cpp
+++ src/wallet/api/wallet.cpp
@@ -2370,13 +2370,14 @@ PendingTransaction* WalletImpl::stakePending(const std::string& sn_key_str, cons
   /// Note(maxim): need to be careful to call `WalletImpl::disposeTransaction` when it is no longer needed
   PendingTransactionImpl * transaction = new PendingTransactionImpl(*this);
 
-  wallet2::stake_result stake_result = m_wallet->create_stake_tx(transaction->m_pending_tx, sn_key, addr_info, amount);
-  if (stake_result != wallet2::stake_result::success)
+  tools::wallet2::stake_result stake_result = m_wallet->create_stake_tx(sn_key, addr_info, amount);
+  if (stake_result.status != tools::wallet2::stake_result_status::success)
   {
     error_msg = "Failed to create a stake transaction: " + stake_result.msg;
     return nullptr;
   }
 
+  transaction->m_pending_tx = {stake_result.ptx};
   return transaction;
 }
 
