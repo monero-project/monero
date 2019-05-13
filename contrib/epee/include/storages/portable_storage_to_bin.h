32c38349483dbe7f0fdd83aa77215aac381b219d
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-05-13 18:58:00 +0000

    storages: fix writing varints on big endian

--- contrib/epee/include/storages/portable_storage_to_bin.h
+++ contrib/epee/include/storages/portable_storage_to_bin.h
@@ -40,7 +40,7 @@ namespace epee
     template<class pack_value, class t_stream>
     size_t pack_varint_t(t_stream& strm, uint8_t type_or, size_t& pv)
     {
-      pack_value v = (*((pack_value*)&pv)) << 2;
+      pack_value v = pv << 2;
       v |= type_or;
       strm.write((const char*)&v, sizeof(pack_value));
       return sizeof(pack_value);
