a58a8919e63db5db2d3e46fa65779045620d4fa2
Harrison Hesslink <harrisonhesslink24@gmail.com>
2019-06-02 23:05:06 -0700

    Doxygen Triton changes

--- utils/doxygen-publish.sh
+++ utils/doxygen-publish.sh
@@ -7,7 +7,7 @@ if [[ ! -r "Doxyfile" ]] ; then
 	exit 1
 fi
 
-wwwdir="$HOME/monero-www/"
+wwwdir="$HOME/triton-www/"
 if [[ ! -w "$wwwdir" ]] ; then
 	echo "Error, can not write into wwwdir=$wwwdir. It should be a directory readable/connected to your webserver, or a symlink to such directory"
 	exit 1
@@ -19,7 +19,7 @@ if [[ ! -d "$wwwdir/doc" ]] ; then
 fi
 
 echo "Generating:"
-doxygen Doxyfile && echo "Backup previous version:" && rm -rf ~/monero-www-previous && mv "$wwwdir/doc" ~/monero-www-previous && cp -ar doc/ "$wwwdir/" && echo "Done, builded and copied to public - the doxygen docs" && echo "size:" && du -Dsh "$wwwdir/" && echo "files:" && find "$wwwdir/" | wc -l
+doxygen Doxyfile && echo "Backup previous version:" && rm -rf ~/triton-www-previous && mv "$wwwdir/doc" ~/triton-www-previous && cp -ar doc/ "$wwwdir/" && echo "Done, builded and copied to public - the doxygen docs" && echo "size:" && du -Dsh "$wwwdir/" && echo "files:" && find "$wwwdir/" | wc -l
 
 
 
