079bd4470565e1a5bcdd33be2bb698beb631988f
Harrison Hesslink <harrisonhesslink24@gmail.com>
2019-06-04 11:43:29 -0700

    Change TX Version 2 -> 3

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -42,7 +42,7 @@
 #define CRYPTONOTE_MAX_TX_SIZE                          1000000000
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
-#define CURRENT_TRANSACTION_VERSION                     2
+#define CURRENT_TRANSACTION_VERSION                     3
 #define CURRENT_BLOCK_MAJOR_VERSION                     1
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              500
