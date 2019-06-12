01ced20eca837d111b8134d855cf382241ffceef
Howard Chu <hyc@symas.com>
2019-06-12 10:20:44 +0100

    Delete redundant cppzmq dependency

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl zeromq cppzmq expat ldns cppzmq readline libiconv hidapi protobuf libusb
+packages:=boost openssl zeromq cppzmq expat ldns readline libiconv hidapi protobuf libusb
 native_packages := native_ccache native_protobuf
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
