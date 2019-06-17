63e687c771565e436af470cd279c873bd2e44a4f
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-06-17 09:35:37 +0000

    fuzz_tests: add a boost::regex test

--- contrib/fuzz_testing/fuzz.sh
+++ contrib/fuzz_testing/fuzz.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 AFLFUZZ=$(which afl-fuzz)
+TESTS="block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof|regex"
 if ! test -x "$AFLFUZZ"
 then
   echo "afl-fuzz not found - install american-fuzzy-lop"
@@ -10,12 +11,12 @@ fi
 type="$1"
 if test -z "$type"
 then
-  echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof"
+  echo "usage: $0 $TESTS"
   exit 1
 fi
 case "$type" in
-  block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof|utf8) ;;
-  *) echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof|utf8"; exit 1 ;;
+  block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof|utf8|regex) ;;
+  *) echo "usage: $0 block|transaction|signature|cold-outputs|cold-transaction|load-from-binary|load-from-json|base58|parse-url|http-client|levin|bulletproof|utf8|regex"; exit 1 ;;
 esac
 
 if test -d "fuzz-out/$type"
---
+++ tests/fuzz/regex.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2017-2019, The Monero Project
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <boost/regex.hpp>
+#include "include_base_utils.h"
+#include "file_io_utils.h"
+#include "reg_exp_definer.h"
+#include "fuzzer.h"
+
+class RegexFuzzer: public Fuzzer
+{
+public:
+  RegexFuzzer() {}
+  virtual int init();
+  virtual int run(const std::string &filename);
+};
+
+int RegexFuzzer::init()
+{
+  return 0;
+}
+
+int RegexFuzzer::run(const std::string &filename)
+{
+  using namespace epee;
+
+  std::string s;
+
+  if (!epee::file_io_utils::load_file_to_string(filename, s))
+  {
+    std::cout << "Error: failed to load file " << filename << std::endl;
+    return 1;
+  }
+  STATIC_REGEXP_EXPR_1(rexp_match_gzip, "^.*?((gzip)|(deflate))", boost::regex::icase | boost::regex::normal);
+  boost::smatch result;
+  boost::regex_search(s, result, rexp_match_gzip, boost::match_default);
+  return 0;
+}
+
+int main(int argc, const char **argv)
+{
+  TRY_ENTRY();
+  RegexFuzzer fuzzer;
+  return run_fuzzer(argc, argv, fuzzer);
+  CATCH_ENTRY_L0("main", 1);
+}
+
