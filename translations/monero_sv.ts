783d269f31b4acde128e641d9652391d68b238b9
Jake Hemmerle <jakehemmerle@gmail.com>
2019-08-28 09:43:17 -0400

    typo fix

--- translations/monero_sv.ts
+++ translations/monero_sv.ts
@@ -2197,7 +2197,7 @@ din plånbok igen (din plånboks nycklar är dock INTE hotade i vilket fall som 
     </message>
     <message>
         <location filename="../src/simplewallet/simplewallet.cpp" line="4761"/>
-        <source>With background mining enabled, the daemon will mine when idle and not on batttery.</source>
+        <source>With background mining enabled, the daemon will mine when idle and not on battery.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
