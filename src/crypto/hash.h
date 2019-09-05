928f4be9531a40fcca92d9f878e08f8111254990
Lev Sizov <levsizov2015@outlook.com>
2019-09-05 17:37:19 +0200

    Make null hash constants constexpr
    Simplify m_template initialization in miner

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -89,8 +89,8 @@ namespace crypto {
     epee::to_hex::formatted(o, epee::as_byte_span(v)); return o;
   }
 
-  const static crypto::hash null_hash = {};
-  const static crypto::hash8 null_hash8 = {};
+  constexpr static crypto::hash null_hash = {};
+  constexpr static crypto::hash8 null_hash8 = {};
 }
 
 CRYPTO_MAKE_HASHABLE(hash)
