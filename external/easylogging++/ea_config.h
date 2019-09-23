5bcbd974888b21af3ff46814cccef2071ffc6ceb
Martijn Otto <git@martijnotto.nl>
2019-09-23 16:30:23 +0200

    Disable easylogging crash log on non-glibc libraries
    - easylogging assumes certain non-standard headers and functions
    - these function only exist in glibc
    - compiling under linux without glibc thus broke compilation

--- external/easylogging++/ea_config.h
+++ external/easylogging++/ea_config.h
@@ -9,7 +9,7 @@
 #define ELPP_UTC_DATETIME
 
 #ifdef EASYLOGGING_CC
-#if !(!defined __GNUC__ || defined __MINGW32__ || defined __MINGW64__ || defined __ANDROID__)
+#if !(!defined __GLIBC__ || !defined __GNUC__ || defined __MINGW32__ || defined __MINGW64__ || defined __ANDROID__)
 #define ELPP_FEATURE_CRASH_LOG
 #endif
 #endif
