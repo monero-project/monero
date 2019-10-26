f7f7513ec3f2545a8e5e2d83a7590a290e4199ea
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-10-26 00:05:21 +0000

    rpc: fix PRIx64 build error on some systems

--- src/rpc/rpc_payment_signature.cpp
+++ src/rpc/rpc_payment_signature.cpp
@@ -26,7 +26,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <inttypes.h>
+#include <cinttypes>
 #include <stdlib.h>
 #include <chrono>
 #include "include_base_utils.h"
