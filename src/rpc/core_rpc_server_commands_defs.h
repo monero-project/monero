2f8f3a9432a23264329d6532d4ef630272ac2f56
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-10-27 16:13:46 +0000

    rpc: base flush_cache request/response on the new base structs

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -2556,22 +2556,21 @@ namespace cryptonote
 
   struct COMMAND_RPC_FLUSH_CACHE
   {
-    struct request_t
+    struct request_t: public rpc_request_base
     {
       bool bad_txs;
 
       BEGIN_KV_SERIALIZE_MAP()
+        KV_SERIALIZE_PARENT(rpc_request_base)
         KV_SERIALIZE_OPT(bad_txs, false)
       END_KV_SERIALIZE_MAP()
     };
     typedef epee::misc_utils::struct_init<request_t> request;
 
-    struct response_t
+    struct response_t: public rpc_response_base
     {
-      std::string status;
-
       BEGIN_KV_SERIALIZE_MAP()
-        KV_SERIALIZE(status)
+        KV_SERIALIZE_PARENT(rpc_response_base)
       END_KV_SERIALIZE_MAP()
     };
     typedef epee::misc_utils::struct_init<response_t> response;
