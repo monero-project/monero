c96b7ee61910ab64e0e0cbd978addfe2ea3e4b27
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-10-29 10:43:31 +0000

    tx_pool: fix error message assuming incorrectly

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -264,7 +264,7 @@ namespace cryptonote
         }
         catch (const std::exception &e)
         {
-          MERROR("transaction already exists at inserting in memory pool: " << e.what());
+          MERROR("Error adding transaction to txpool: " << e.what());
           return false;
         }
         tvc.m_verifivation_impossible = true;
@@ -310,7 +310,7 @@ namespace cryptonote
       }
       catch (const std::exception &e)
       {
-        MERROR("internal error: transaction already exists at inserting in memory pool: " << e.what());
+        MERROR("internal error: error adding transaction to txpool: " << e.what());
         return false;
       }
       tvc.m_added_to_pool = true;
