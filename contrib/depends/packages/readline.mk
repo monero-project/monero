436e4c336396f12c36df779c973824bbbae386ae
Howard Chu <hyc@symas.com>
2019-11-03 00:12:31 +0000

    Fix readline build
    Make sure it links to our libtinfo from our ncurses build.
    Hardcode some basic terminal descriptions into our libtinfo.
    Re-enable $HOME/.terminfo support to allow user customization.
    Use unlikely terminfo-dir, to prevent accidentally using
    differently-configured system databases.

--- contrib/depends/packages/readline.mk
+++ contrib/depends/packages/readline.mk
@@ -7,7 +7,7 @@ $(package)_dependencies=ncurses
 
 define $(package)_set_vars
   $(package)_build_opts=CC="$($(package)_cc)"
-  $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
+  $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)" LDFLAGS="-L$(host_prefix)/lib"
   $(package)_config_env_darwin=RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin11-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin11-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
   $(package)_config_opts+=--prefix=$(host_prefix)
   $(package)_config_opts+=--exec-prefix=$(host_prefix)
