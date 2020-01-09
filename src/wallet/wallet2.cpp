f507a43a239358b1645f383f6974f0862de2999b
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-01-09 12:48:57 +0000

    wallet2: do not remove known rings when a tx fails
    Even if it fails, the ring composition is known to a potential
    adversary, and so we should reuse the same ring next time

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -2907,7 +2907,6 @@ void wallet2::update_pool_state(std::vector<std::pair<cryptonote::transaction, b
         pit->second.m_state = wallet2::unconfirmed_transfer_details::failed;
 
         // the inputs aren't spent anymore, since the tx failed
-        remove_rings(pit->second.m_tx);
         for (size_t vini = 0; vini < pit->second.m_tx.vin.size(); ++vini)
         {
           if (pit->second.m_tx.vin[vini].type() == typeid(txin_to_key))
