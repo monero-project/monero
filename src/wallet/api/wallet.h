378ddc7f42ef6dcc18838f9cd98692eeb081e152
xiphon <xiphon@protonmail.com>
2020-02-11 23:27:30 +0000

    wallet2_api: wallet recovery - seed offset passphrase support

--- src/wallet/api/wallet.h
+++ src/wallet/api/wallet.h
@@ -60,7 +60,7 @@ public:
                             const std::string &language) const override;
     bool open(const std::string &path, const std::string &password);
     bool recover(const std::string &path,const std::string &password,
-                            const std::string &seed);
+                            const std::string &seed, const std::string &seed_offset = {});
     bool recoverFromKeysWithPassword(const std::string &path,
                             const std::string &password,
                             const std::string &language,
