332d60719a78875206f8231b1a2429335e4361e6
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-06-20 15:32:45 +0000

    tx_pool: mine stem txes in fake chain mode
    This fixes the functional tests, since txes would not be mined
    after being sent to the daemon (they'd be waiting for the
    dandelion timeout first)

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -650,7 +650,7 @@ namespace cryptonote
     r = m_blockchain_storage.init(db.release(), m_nettype, m_offline, regtest ? &regtest_test_options : test_options, fixed_difficulty, get_checkpoints);
     CHECK_AND_ASSERT_MES(r, false, "Failed to initialize blockchain storage");
 
-    r = m_mempool.init(max_txpool_weight);
+    r = m_mempool.init(max_txpool_weight, m_nettype == FAKECHAIN);
     CHECK_AND_ASSERT_MES(r, false, "Failed to initialize memory pool");
 
     // now that we have a valid m_blockchain_storage, we can clean out any
