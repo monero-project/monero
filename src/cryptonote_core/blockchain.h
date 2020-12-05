d25b0b6ebdc05f4b05f531478bc52f9219aee333
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-12-05 23:51:09 +0000

    blockchain: only update pruning when the chain changed
    Most of the addition calls will end up adding nothing, since we
    expect to receive every block from all peers, and only the first
    time we get it leads to actual addition.

--- src/cryptonote_core/blockchain.h
+++ src/cryptonote_core/blockchain.h
@@ -1149,6 +1149,7 @@ namespace cryptonote
 
 
     bool m_batch_success;
+    bool m_update_pruning;
 
     /* `boost::function` is used because the implementation never allocates if
        the callable object has a single `std::shared_ptr` or `std::weap_ptr`
