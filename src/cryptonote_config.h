ef1524eccacb9acb62ddbf674f9ad1d0ffb2b588
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-12-10 19:26:12 +0000

    protocol: add a sanity check to the number of block hashes sent

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -93,6 +93,7 @@
 
 
 #define BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT          10000  //by default, blocks ids count in synchronizing
+#define BLOCKS_IDS_SYNCHRONIZING_MAX_COUNT              25000  //max blocks ids count in synchronizing
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT_PRE_V4       100    //by default, blocks count in blocks downloading
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT              20     //by default, blocks count in blocks downloading
 #define BLOCKS_SYNCHRONIZING_MAX_COUNT                  2048   //must be a power of 2, greater than 128, equal to SEEDHASH_EPOCH_BLOCKS
