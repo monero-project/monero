ba60541f3e38087f345f64dd4168531703934cdf
selsta <selsta@sent.at>
2020-12-23 02:13:16 +0100

    build: prepare v0.17.1.8

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5376,7 +5376,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "9df68e5d89dc3789768c4701c5dea3fd32c1df22acc439c29b775eb54cefbf29";
+static const char expected_block_hashes_hash[] = "9fcc97979d033efad4d6513ff20195f0caaa20fa878a088d6b28a87402354bc1";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
