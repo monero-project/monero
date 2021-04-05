6e22710f0e1e09e3c552743b2263ddaec06453b3
benevanoff <benjaminevanoff99@gmail.com>
2021-04-05 13:41:04 -0500

    expose set_offline to wallet api

--- src/wallet/api/wallet2_api.h
+++ src/wallet/api/wallet2_api.h
@@ -1014,6 +1014,12 @@ struct Wallet
     * \return true on success
     */
     virtual bool rescanSpent() = 0;
+
+   /*
+    * \brief setOffline - toggle set offline on/off
+    * \param offline - true/false
+    */
+    virtual void setOffline(bool offline) = 0;
     
     //! blackballs a set of outputs
     virtual bool blackballOutputs(const std::vector<std::string> &outputs, bool add) = 0;
