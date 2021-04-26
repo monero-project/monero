a395f363e79f1886c8cfd8ff2f45c171a64da39b
selsta <selsta@sent.at>
2021-04-26 19:54:25 +0200

    depends, docker: fix bintray links

--- contrib/depends/packages/boost.mk
+++ contrib/depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost                                                                                                                                                                                                                      
 $(package)_version=1_64_0
-$(package)_download_path=https://dl.bintray.com/boostorg/release/1.64.0/source/
+$(package)_download_path=https://downloads.sourceforge.net/project/boost/boost/1.64.0/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
 $(package)_dependencies=libiconv
