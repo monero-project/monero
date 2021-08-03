3b03a4f02d30bafda9ec3881e4fb1a1bc7345d0b
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2021-08-03 11:19:56 +0000

    serialization: add boost::optional serialization

---
+++ src/serialization/optional.h
@@ -0,0 +1,79 @@
+// Copyright (c) 2014-2020, The Monero Project
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
+
+/*! \file variant.h
+ *
+ * \brief for dealing with variants
+ *
+ * \detailed Variant: OOP Union
+ */
+#pragma once
+
+#include <boost/optional.hpp>
+#include "serialization.h"
+
+// read
+template <template <bool> class Archive, typename T>
+bool do_serialize(Archive<false> &ar, boost::optional<T> &v)
+{
+  uint8_t present = 0;
+
+  ar.serialize_uint(present);
+  if (!ar.stream().good())
+    return false;
+
+  if (present)
+  {
+    v = T();
+    if (!::do_serialize(ar, *v))
+      return false;
+  }
+  else
+  {
+    v = boost::none;
+  }
+
+  return true;
+}
+
+// write
+template <template <bool> class Archive, typename T>
+bool do_serialize(Archive<true> &ar, boost::optional<T> &v)
+{
+  const uint8_t present = v != boost::none;
+  ar.serialize_uint(present);
+  if (present)
+  {
+    if (!::do_serialize(ar, *v))
+      return false;
+  }
+
+  return true;
+}
