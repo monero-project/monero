f5086126ca5bea3bee92545b50ae906673293bd9
Howard Chu <hyc@symas.com>
2021-09-01 11:30:41 +0100

    gitian: cleanup more unused packages
    These appear to be leftovers inherited from Bitcoin's builder

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -11,8 +11,8 @@ hardware_native_packages := native_protobuf
 android_native_packages = android_ndk
 android_packages = ncurses readline sodium
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias $(hardware_native_packages)
-darwin_packages = sodium ncurses readline $(hardware_packages)
+darwin_native_packages = $(hardware_native_packages)
+darwin_packages = ncurses readline sodium $(hardware_packages)
 
 # not really native...
 freebsd_native_packages = freebsd_base
@@ -34,6 +34,6 @@ mingw32_packages = icu4c sodium $(hardware_packages)
 mingw32_native_packages = $(hardware_native_packages)
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools
 endif
 
