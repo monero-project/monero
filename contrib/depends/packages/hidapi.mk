fe5c2aec35970c545f50679a06d9231b13104c5a
Howard Chu <hyc@symas.com>
2022-05-04 06:58:24 +0100

    Fix arm64 darwin package defs

--- contrib/depends/packages/hidapi.mk
+++ contrib/depends/packages/hidapi.mk
@@ -9,7 +9,7 @@ $(package)_patches=missing_win_include.patch
 define $(package)_set_vars
 $(package)_config_opts=--enable-static --disable-shared
 $(package)_config_opts+=--prefix=$(host_prefix)
-$(package)_config_opts_darwin+=RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin11-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin11-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
+$(package)_config_opts_darwin+=RANLIB="$(host_prefix)/native/bin/$(host)-ranlib" AR="$(host_prefix)/native/bin/$(host)-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
 $(package)_config_opts_linux+=libudev_LIBS="-L$(host_prefix)/lib -ludev"
 $(package)_config_opts_linux+=libudev_CFLAGS=-I$(host_prefix)/include
 $(package)_config_opts_linux+=libusb_LIBS="-L$(host_prefix)/lib -lusb-1.0"
