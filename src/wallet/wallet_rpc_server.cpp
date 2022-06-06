15f878e593d079bb923bd7e82a6d9cf5ed309e58
Harrison Hesslink <harrisonhesslink24@gmail.com>
2022-06-06 16:58:41 -0700

    Incorrect Password

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -3390,7 +3390,7 @@ namespace tools
     if (!wal)
     {
       er.code = WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR;
-      er.message = "Failed to open wallet";
+      er.message = "Incorrect Password!";
       return false;
     }
 
