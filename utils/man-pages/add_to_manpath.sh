3650cabbe4b154000eb4861b832714213e47169a
Speer <speer1996@i2pmail.org>
2022-07-01 21:09:08 +0300

    Move monerod's manual to section 8

--- utils/man-pages/add_to_manpath.sh
+++ utils/man-pages/add_to_manpath.sh
@@ -1,8 +1,10 @@
-files=monero*.1
+files=monero*.[1-8]
 
 for f in $files
 do
-    out=/usr/local/man/man1/$(basename $f)
+    section=${f##*.}
+    mkdir /usr/local/man/man$section
+    out=/usr/local/man/man$section/$(basename $f)
     echo  $f "** copying to MANPATH"
     cp $f $out
     gzip $out
