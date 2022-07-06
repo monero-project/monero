dba93af21f7b6ba49ce03ce2ac8b2039d65ad7bb
woodser <woodser@protonmail.com>
2022-07-06 02:21:04 -0400

    speed up testnet hardforks for local development (#5)

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -82,24 +82,24 @@ const hardfork_t testnet_hard_forks[] = {
   { 1, 1, 0, 1341378000 },
 
   // version 2 starts from block 624634, which is on or around the 23rd of November, 2015. Fork time finalised on 2015-11-20. No fork voting occurs for the v2 fork.
-  { 2, 624634, 0, 1445355000 },
+  { 2, 10, 0, 1445355000 },
 
   // versions 3-5 were passed in rapid succession from September 18th, 2016
-  { 3, 800500, 0, 1472415034 },
-  { 4, 801219, 0, 1472415035 },
-  { 5, 802660, 0, 1472415036 + 86400*180 }, // add 5 months on testnet to shut the update warning up since there's a large gap to v6
+  { 3, 20, 0, 1472415034 },
+  { 4, 30, 0, 1472415035 },
+  { 5, 40, 0, 1472415036 + 86400*180 }, // add 5 months on testnet to shut the update warning up since there's a large gap to v6
 
-  { 6, 971400, 0, 1501709789 },
-  { 7, 1057027, 0, 1512211236 },
-  { 8, 1057058, 0, 1533211200 },
-  { 9, 1057778, 0, 1533297600 },
-  { 10, 1154318, 0, 1550153694 },
-  { 11, 1155038, 0, 1550225678 },
-  { 12, 1308737, 0, 1569582000 },
-  { 13, 1543939, 0, 1599069376 },
-  { 14, 1544659, 0, 1599069377 },
-  { 15, 1982800, 0, 1652727000 },
-  { 16, 1983520, 0, 1652813400 },
+  { 6, 50, 0, 1501709789 },
+  { 7, 60, 0, 1512211236 },
+  { 8, 70, 0, 1533211200 },
+  { 9, 80, 0, 1533297600 },
+  { 10, 90, 0, 1550153694 },
+  { 11, 100, 0, 1550225678 },
+  { 12, 110, 0, 1569582000 },
+  { 13, 120, 0, 1599069376 },
+  { 14, 130, 0, 1599069377 },
+  { 15, 140, 0, 1652727000 },
+  { 16, 150, 0, 1652813400 },
 };
 const size_t num_testnet_hard_forks = sizeof(testnet_hard_forks) / sizeof(testnet_hard_forks[0]);
 const uint64_t testnet_hard_fork_version_1_till = 624633;
