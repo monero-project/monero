15384cc1353f434315ab5244bb59f99d937d810f
tobtoht <tob@featherwallet.org>
2023-01-18 19:21:13 +0100

    depends: bump hidapi/libusb
    fixes Trezor connectivity issues on Windows 10/11
    
    Co-authored-by: selsta <selsta@sent.at>

--- contrib/depends/packages/libusb.mk
+++ contrib/depends/packages/libusb.mk
@@ -1,8 +1,8 @@
 package=libusb
-$(package)_version=1.0.22
-$(package)_download_path=https://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-$($(package)_version)/
+$(package)_version=1.0.26
+$(package)_download_path=https://github.com/libusb/libusb/releases/download/v$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=75aeb9d59a4fdb800d329a545c2e6799f732362193b465ea198f2aa275518157
+$(package)_sha256_hash=12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5
 
 define $(package)_preprocess_cmds
   autoreconf -i
