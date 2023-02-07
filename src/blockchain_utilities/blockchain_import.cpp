408079661de7f4b31205e19f8a631d64f8158d94
Jeffrey Ryan <jeffro256@tutanota.com>
2023-02-06 19:06:49 -0600

    serialization: remove unused function dump_json

--- src/blockchain_utilities/blockchain_import.cpp
+++ src/blockchain_utilities/blockchain_import.cpp
@@ -40,7 +40,6 @@
 #include "blocks/blocks.h"
 #include "cryptonote_basic/cryptonote_format_utils.h"
 #include "serialization/binary_utils.h" // dump_binary(), parse_binary()
-#include "serialization/json_utils.h" // dump_json()
 #include "include_base_utils.h"
 #include "cryptonote_core/cryptonote_core.h"
 
