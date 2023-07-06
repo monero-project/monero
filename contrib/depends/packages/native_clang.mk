ebb7671e0ea390d3b25df6fa9a6b564321eba7a6
tobtoht <tob@featherwallet.org>
2023-07-07 00:13:12 +0200

    depends: use macOS 11 SDK with libc++ headers

--- contrib/depends/packages/native_clang.mk
+++ contrib/depends/packages/native_clang.mk
@@ -24,6 +24,5 @@ define $(package)_stage_cmds
   cp lib/libLTO.so $($(package)_staging_prefix_dir)/lib/ && \
   cp -rf lib/clang/$($(package)_version)/include/* $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include/ && \
   cp bin/dsymutil $($(package)_staging_prefix_dir)/bin/$(host)-dsymutil && \
-  if `test -d include/c++/`; then cp -rf include/c++/ $($(package)_staging_prefix_dir)/include/; fi && \
   if `test -d lib/c++/`; then cp -rf lib/c++/ $($(package)_staging_prefix_dir)/lib/; fi
 endef
