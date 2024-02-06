052df1b28ca89b912258ac573516dad5325344e8
Lee *!* Clagett <code@leeclagett.com>
2024-02-06 12:47:47 -0500

    Zero initialize rctSigBase elements

--- src/ringct/rctTypes.h
+++ src/ringct/rctTypes.h
@@ -325,6 +325,10 @@ namespace rct {
         ctkeyV outPk;
         xmr_amount txnFee; // contains b
 
+        rctSigBase() :
+          type(RCTTypeNull), message{}, mixRing{}, pseudoOuts{}, ecdhInfo{}, outPk{}, txnFee(0)
+        {}
+
         template<bool W, template <bool> class Archive>
         bool serialize_rctsig_base(Archive<W> &ar, size_t inputs, size_t outputs)
         {
