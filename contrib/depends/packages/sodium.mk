35477a66c2b1b717cc4530e1bb92bd227e1c0e0e
tobtoht <tob@featherwallet.org>
2024-02-13 22:47:07 +0100

    build: remove glibc backcompat

--- contrib/depends/packages/sodium.mk
+++ contrib/depends/packages/sodium.mk
@@ -3,7 +3,7 @@ $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=libsodium-$($(package)_version).tar.gz
 $(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
-$(package)_patches=disable-glibc-getrandom-getentropy.patch fix-whitespace.patch
+$(package)_patches=fix-whitespace.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--enable-static --disable-shared
@@ -11,7 +11,6 @@ $(package)_config_opts+=--prefix=$(host_prefix)
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/disable-glibc-getrandom-getentropy.patch &&\
   autoconf &&\
   patch -p1 < $($(package)_patch_dir)/fix-whitespace.patch
 endef
