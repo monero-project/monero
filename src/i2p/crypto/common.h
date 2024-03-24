f67b0578a995a5cf68bb99b345b136eac58edc65
preland <89992615+preland@users.noreply.github.com>
2024-03-24 13:34:13 -0500

    "copy-pasting only 500 lines of code"

---
+++ src/i2p/crypto/common.h
@@ -0,0 +1,85 @@
+// Copyright (c) 2014-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CRYPTO_COMMON_H
+#define BITCOIN_CRYPTO_COMMON_H
+
+#include "../compat/endian.h"
+
+#include <cstdint>
+#include <cstring>
+
+uint16_t static inline ReadLE16(const unsigned char* ptr)
+{
+    uint16_t x;
+    memcpy(&x, ptr, 2);
+    return le16toh_internal(x);
+}
+
+uint32_t static inline ReadLE32(const unsigned char* ptr)
+{
+    uint32_t x;
+    memcpy(&x, ptr, 4);
+    return le32toh_internal(x);
+}
+
+uint64_t static inline ReadLE64(const unsigned char* ptr)
+{
+    uint64_t x;
+    memcpy(&x, ptr, 8);
+    return le64toh_internal(x);
+}
+
+void static inline WriteLE16(unsigned char* ptr, uint16_t x)
+{
+    uint16_t v = htole16_internal(x);
+    memcpy(ptr, &v, 2);
+}
+
+void static inline WriteLE32(unsigned char* ptr, uint32_t x)
+{
+    uint32_t v = htole32_internal(x);
+    memcpy(ptr, &v, 4);
+}
+
+void static inline WriteLE64(unsigned char* ptr, uint64_t x)
+{
+    uint64_t v = htole64_internal(x);
+    memcpy(ptr, &v, 8);
+}
+
+uint16_t static inline ReadBE16(const unsigned char* ptr)
+{
+    uint16_t x;
+    memcpy(&x, ptr, 2);
+    return be16toh_internal(x);
+}
+
+uint32_t static inline ReadBE32(const unsigned char* ptr)
+{
+    uint32_t x;
+    memcpy(&x, ptr, 4);
+    return be32toh_internal(x);
+}
+
+uint64_t static inline ReadBE64(const unsigned char* ptr)
+{
+    uint64_t x;
+    memcpy(&x, ptr, 8);
+    return be64toh_internal(x);
+}
+
+void static inline WriteBE32(unsigned char* ptr, uint32_t x)
+{
+    uint32_t v = htobe32_internal(x);
+    memcpy(ptr, &v, 4);
+}
+
+void static inline WriteBE64(unsigned char* ptr, uint64_t x)
+{
+    uint64_t v = htobe64_internal(x);
+    memcpy(ptr, &v, 8);
+}
+
+#endif // BITCOIN_CRYPTO_COMMON_H
