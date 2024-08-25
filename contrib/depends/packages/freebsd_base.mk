6c374721ed9c623f26a5d4810602ff2c4ee53b1a
tobtoht <tob@featherwallet.org>
2024-08-25 17:45:37 +0200

    depends: freebsd_base: update to 12.3

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -1,9 +1,9 @@
 package=freebsd_base
-$(package)_version=11.3
-$(package)_download_path=https://download.freebsd.org/ftp/releases/amd64/$($(package)_version)-RELEASE/
-$(package)_download_file=base.txz
+$(package)_version=12.3
+$(package)_download_path=https://ci-mirrors.rust-lang.org/rustc
+$(package)_download_file=2022-05-06-freebsd-$($(package)_version)-amd64-base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
-$(package)_sha256_hash=4599023ac136325b86f2fddeec64c1624daa83657e40b00b2ef944c81463a4ff
+$(package)_sha256_hash=e85b256930a2fbc04b80334106afecba0f11e52e32ffa197a88d7319cf059840
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
