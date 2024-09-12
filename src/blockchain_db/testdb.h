4f295ce02c57cbea5c358d7e7acb02f4f0140122
j-berman <justinberman@protonmail.com>
2024-09-12 15:03:35 -0700

    fcmp++: key image migration
    - Removing the sign bit from key images enables an optimization for
    fcmp's.
    - If an fcmp includes a key image with sign bit cleared,while the
    same key image with sign bit set exists in the chain already via a
    ring signature, then the fcmp would be a double spend attempt and
    the daemon must be able to detect and reject it.
    - In order for the daemon to detect such double spends, upon
    booting the daemon, we clear the sign bit from all key images
    already in the db. We also make sure that all key images held in
    memory by the pool have sign bit cleared as well.
    - Key images with sign bit cleared are a new type:
    `crypto::key_image_y`. The sign bit can be cleared via
    `crypto::key_image_to_y`.
    - The `_y` denotes that the encoded point is now the point's y
    coordinate.
    - In order to maintain backwards compatibility with current RPC
    consumers, the daemon keeps track of which key images have sign
    bit cleared and not, so that upon serving
    `spent_key_image_info::id_hash`, the daemon can re-construct the
    original key image and serve it to clients.

--- src/blockchain_db/testdb.h
+++ src/blockchain_db/testdb.h
@@ -122,7 +122,7 @@ public:
   virtual std::array<uint8_t, 32UL> get_tree_root() const override { return {}; };
   virtual uint64_t get_num_leaf_tuples() const override { return 0; };
 
-  virtual bool for_all_key_images(std::function<bool(const crypto::key_image&)>) const override { return true; }
+  virtual bool for_all_key_images(std::function<bool(const crypto::key_image_y&)>) const override { return true; }
   virtual bool for_blocks_range(const uint64_t&, const uint64_t&, std::function<bool(uint64_t, const crypto::hash&, const cryptonote::block&)>) const override { return true; }
   virtual bool for_all_transactions(std::function<bool(const crypto::hash&, const cryptonote::transaction&)>, bool pruned) const override { return true; }
   virtual bool for_all_outputs(std::function<bool(uint64_t amount, const crypto::hash &tx_hash, uint64_t height, size_t tx_idx)> f) const override { return true; }
