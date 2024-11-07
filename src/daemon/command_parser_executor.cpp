a1b545a2f7b2ac48d66e7fd846ca5093286525c7
jeffro256 <jeffro256@tutanota.com>
2024-11-07 00:34:59 -0600

    p2p: allow comments in banlist files
    In-line comments explicitly explaining banned hosts/subnets might help assuage fears of some good banlists' arbitaryiness.

--- src/daemon/command_parser_executor.cpp
+++ src/daemon/command_parser_executor.cpp
@@ -696,6 +696,16 @@ bool t_command_parser_executor::ban(const std::vector<std::string>& args)
       std::ifstream ifs(ban_list_path.string());
       for (std::string line; std::getline(ifs, line); )
       {
+        // ignore comments after '#' character
+        const size_t pound_idx = line.find('#');
+        if (pound_idx != std::string::npos)
+          line.resize(pound_idx);
+
+        // trim whitespace and ignore empty lines
+        boost::trim(line);
+        if (line.empty())
+          continue;
+
         auto subnet = net::get_ipv4_subnet_address(line);
         if (subnet)
         {
