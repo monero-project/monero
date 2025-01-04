040c16321f2249bc4496af04abb202501f536966
tobtoht <tob@featherwallet.org>
2025-01-04 16:57:31 +0100

    depends: remove expat

--- contrib/depends/packages/unbound.mk
+++ contrib/depends/packages/unbound.mk
@@ -3,11 +3,12 @@ $(package)_version=1.22.0
 $(package)_download_path=https://www.nlnetlabs.nl/downloads/$(package)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=c5dd1bdef5d5685b2cedb749158dd152c52d44f65529a34ac15cd88d4b1b3d43
-$(package)_dependencies=openssl expat
+$(package)_dependencies=openssl
+$(package)_patches=no-expat.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --enable-static --without-pyunbound --prefix=$(host_prefix)
-  $(package)_config_opts+=--with-libexpat=$(host_prefix) --with-ssl=$(host_prefix) --with-libevent=no
+  $(package)_config_opts+=--with-libexpat=no --with-ssl=$(host_prefix) --with-libevent=no
   $(package)_config_opts+=--without-pythonmodule --disable-flto --with-pthreads --with-libunbound-only
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_w64=--enable-static-exe --sysconfdir=/etc --prefix=$(host_prefix) --target=$(host_prefix)
@@ -18,6 +19,7 @@ endef
 
 # Remove blobs
 define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/no-expat.patch &&\
   rm configure~ doc/*.odp doc/*.pdf contrib/*.tar.gz contrib/*.tar.bz2 &&\
   rm -rf testdata dnscrypt/testdata
 endef
