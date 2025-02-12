952486e5bcadcdda1da95bf16bde8c5bc9f5c9e0
U65535F <132809543+U65535F@users.noreply.github.com>
2025-02-12 18:45:59 +0530

    add const keyword to wallet2 interface

--- src/wallet/api/wallet2_api.h
+++ src/wallet/api/wallet2_api.h
@@ -1069,7 +1069,7 @@ struct Wallet
     virtual bool parse_uri(const std::string &uri, std::vector<std::string> &addresses, std::vector<std::uint64_t> &amounts, std::vector<std::string> &recipient_names, std::string &payment_id, std::string &tx_description, std::vector<std::string> &unknown_parameters, std::string &error);
     
     virtual std::string make_uri(const std::string &address, const std::string &payment_id, uint64_t amount, const std::string &tx_description, const std::string &recipient_name, std::string &error) const = 0;
-    virtual std::string make_uri(std::vector<std::string> &addresses, std::vector<std::uint64_t> &amounts, std::vector<std::string> &recipient_names, const std::string &payment_id, const std::string &tx_description, std::string &error) const;
+    virtual std::string make_uri(const std::vector<std::string> &addresses, const std::vector<std::uint64_t> &amounts, const std::vector<std::string> &recipient_names, const std::string &payment_id, const std::string &tx_description, std::string &error) const;
     virtual std::string getDefaultDataDir() const = 0;
    
    /*
