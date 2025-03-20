2aa90a11350a46ff0ed4de7f91ec9fd5c96b6d3e
meed3 <meed@dnmx.su>
2025-03-20 12:52:20 +0000

    Add log-level arg description to monerod

--- src/daemon/command_line_args.h
+++ src/daemon/command_line_args.h
@@ -79,7 +79,7 @@ namespace daemon_args
   };
   const command_line::arg_descriptor<std::string> arg_log_level = {
     "log-level"
-  , ""
+  , "0-4 or categories"
   , ""
   };
   const command_line::arg_descriptor<std::vector<std::string>> arg_command = {
