30a14f885a684fe8507dcafb44698553beb8095f
jeffro256 <jeffro256@tutanota.com>
2025-04-07 14:49:24 -0500

    carrot+fcmp: fake_pruned_blockchain builds FCMP trees

--- tests/unit_tests/fake_pruned_blockchain.h
+++ tests/unit_tests/fake_pruned_blockchain.h
@@ -31,6 +31,7 @@
 #define IN_UNIT_TESTS
 
 #include "carrot_impl/carrot_tx_builder_types.h"
+#include "curve_trees.h"
 #include "wallet/wallet2.h"
 
 namespace mock
@@ -41,13 +42,15 @@ bool construct_miner_tx_fake_reward_1out(const size_t height,
     const rct::xmr_amount reward,
     const cryptonote::account_public_address &miner_address,
     cryptonote::transaction& tx,
-    const uint8_t hf_version);
+    const uint8_t hf_version,
+    const size_t num_tx_outputs = 1);
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 cryptonote::transaction construct_miner_tx_fake_reward_1out(const size_t height,
     const rct::xmr_amount reward,
     const cryptonote::account_public_address &miner_address,
-    const uint8_t hf_version);
+    const uint8_t hf_version,
+    const size_t num_tx_outputs = 1);
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 struct stripped_down_tx_source_entry_t
@@ -70,10 +73,17 @@ cryptonote::tx_source_entry gen_tx_source_entry_fake_members(
 //----------------------------------------------------------------------------------------------------------------------
 cryptonote::transaction construct_pre_carrot_tx_with_fake_inputs(
     const cryptonote::account_keys &sender_account_keys,
-    const std::unordered_map<crypto::public_key, cryptonote::subaddress_index> &subaddresses,
+    const std::unordered_map<crypto::public_key, cryptonote::subaddress_index> &sender_subaddress_map,
     std::vector<stripped_down_tx_source_entry_t> &&stripped_sources,
     std::vector<cryptonote::tx_destination_entry> &destinations,
     const boost::optional<cryptonote::account_public_address> &change_addr,
+    const rct::xmr_amount fee,
+    const uint8_t hf_version,
+    const bool sweep_unmixable_override = false);
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+cryptonote::transaction construct_pre_carrot_tx_with_fake_inputs(
+    std::vector<cryptonote::tx_destination_entry> &destinations,
     const rct::xmr_amount fee,
     const uint8_t hf_version,
     const bool sweep_unmixable_override = false);
@@ -91,29 +101,26 @@ cryptonote::transaction construct_carrot_pruned_transaction_fake_inputs(
 extern const cryptonote::account_public_address null_addr;
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
+using fcmp_generic_object_t = std::unique_ptr<void, decltype(&free)>;
+static inline fcmp_generic_object_t make_fcmp_generic_object(void *p) { return fcmp_generic_object_t(p, &free); }
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 class fake_pruned_blockchain
 {
 public:
     static constexpr rct::xmr_amount miner_reward = 600000000000; // 0.6 XMR
 
     fake_pruned_blockchain(const uint64_t start_block_index = 0,
-            const cryptonote::network_type nettype = cryptonote::MAINNET):
-        m_start_block_index(start_block_index),
-        m_nettype(nettype),
-        m_output_index()
-    {
-        add_starting_block();
-    }
+        const cryptonote::network_type nettype = cryptonote::MAINNET);
 
     void add_block(const uint8_t hf_version,
         std::vector<cryptonote::transaction> &&txs,
-        const cryptonote::account_public_address &miner_address);
+        const cryptonote::account_public_address &miner_address,
+        const size_t num_miner_tx_outputs = 1);
 
     void add_block(cryptonote::block &&blk,
         std::vector<cryptonote::transaction> &&pruned_txs,
         std::vector<crypto::hash> &&prunable_hashes);
-
-    void pop_block();
 
     void get_blocks_data(const uint64_t start_block_index,
         const uint64_t stop_block_index,
@@ -121,6 +128,14 @@ public:
         std::vector<tools::wallet2::parsed_block> &parsed_blocks_out) const;
 
     void init_wallet_for_starting_block(tools::wallet2 &w) const;
+
+    /**
+     * brief: refresh_wallet - pulls block data from chain and calls process_parsed_blocks()
+     * param: w -
+     * param: start_height - start height to pull block data from (inclusive)
+     * return: number of blocks added by process_parsed_blocks()
+     */
+    uint64_t refresh_wallet(tools::wallet2 &w, const size_t start_height) const;
 
     uint8_t hf_version() const
     {
@@ -147,6 +162,11 @@ public:
         return m_parsed_blocks.empty() ? crypto::null_hash : m_parsed_blocks.back().hash;
     }
 
+    uint64_t num_outputs() const
+    {
+        return m_num_outputs;
+    }
+
     tools::wallet2::parsed_block get_parsed_block(const uint64_t block_index) const
     {
         if (block_index >= this->height() || block_index < this->m_start_block_index)
@@ -155,19 +175,27 @@ public:
         return m_parsed_blocks.at(block_index - this->m_start_block_index);
     }
 
-private:
-    void assert_chain_count() const
+    const uint8_t *get_fcmp_tree_root_at(const uint64_t block_index) const
     {
-        CHECK_AND_ASSERT_THROW_MES(m_block_entries.size() == m_parsed_blocks.size(), "blockchain size mismatch");
+        if (block_index >= this->height() || block_index < this->m_start_block_index)
+            throw std::out_of_range("get_fcmp_tree_root_at requested block index");
+
+        return reinterpret_cast<uint8_t*>(m_curve_tree_roots.at(block_index - this->m_start_block_index).get());
     }
+
+private:
+    void assert_chain_count() const;
 
     void add_starting_block();
 
     uint64_t m_start_block_index;
     cryptonote::network_type m_nettype;
-    uint64_t m_output_index;
+    uint64_t m_num_outputs;
     std::vector<cryptonote::block_complete_entry> m_block_entries;
     std::vector<tools::wallet2::parsed_block> m_parsed_blocks;
+    std::vector<fcmp_generic_object_t> m_curve_tree_roots;
+
+    CurveTreesGlobalTree m_global_curve_tree;
 };
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
