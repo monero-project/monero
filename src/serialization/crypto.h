a37299df9488dea5a14869ad6744ccc9dd1786c0
jeffro256 <jeffro256@tutanota.com>
2025-04-19 21:28:52 -0500

    upstream: add serialization for mx25519_pubkey

--- src/serialization/crypto.h
+++ src/serialization/crypto.h
@@ -37,6 +37,7 @@
 #include "crypto/chacha.h"
 #include "crypto/crypto.h"
 #include "crypto/hash.h"
+#include "mx25519.h"
 
 // read
 template <template <bool> class Archive>
@@ -87,6 +88,7 @@ BLOB_SERIALIZER(crypto::key_image);
 BLOB_SERIALIZER(crypto::signature);
 BLOB_SERIALIZER(crypto::view_tag);
 BLOB_SERIALIZER(crypto::ec_point);
+BLOB_SERIALIZER(mx25519_pubkey);
 VARIANT_TAG(debug_archive, crypto::hash, "hash");
 VARIANT_TAG(debug_archive, crypto::hash8, "hash8");
 VARIANT_TAG(debug_archive, crypto::public_key, "public_key");
@@ -96,4 +98,5 @@ VARIANT_TAG(debug_archive, crypto::key_image, "key_image");
 VARIANT_TAG(debug_archive, crypto::signature, "signature");
 VARIANT_TAG(debug_archive, crypto::view_tag, "view_tag");
 VARIANT_TAG(debug_archive, crypto::ec_point, "ec_point");
+VARIANT_TAG(debug_archive, mx25519_pubkey, "mx25519_pubkey");
 
