a2f336d22fb019d71679b5b55e7249f0dbcbdd89
jeffro256 <jeffro256@tutanota.com>
2025-07-16 00:03:39 -0500

    serialization: massage pair casting to remove user-defined-cast warnings
    Similar to: https://github.com/sstsimulator/sst-core/pull/1315

--- src/serialization/container.h
+++ src/serialization/container.h
@@ -132,7 +132,8 @@ bool do_serialize_container(Archive<true> &ar, C &v)
     if (i != v.begin())
       ar.delimit_array();
     using serializable_value_type = typename ::serialization::detail::serializable_value_type<C>::type;
-    if(!::serialization::detail::serialize_container_element(ar, (serializable_value_type&)*i))
+    auto &i_ref = const_cast<serializable_value_type&>(reinterpret_cast<const serializable_value_type&>(*i));
+    if(!::serialization::detail::serialize_container_element(ar, i_ref))
       return false;
     if (!ar.good())
       return false;
