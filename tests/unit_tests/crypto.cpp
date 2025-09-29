c50d41704dfa5a3c98a521e992fce88a843c4f08
j-berman <justinberman@protonmail.com>
2025-09-29 09:14:38 -0700

    @vtnerd suggestion to throw bad_alloc

--- tests/unit_tests/crypto.cpp
+++ tests/unit_tests/crypto.cpp
@@ -354,7 +354,7 @@ TEST(Crypto, batch_inversion)
   {
     fe *ptr = (fe *) malloc(n * sizeof(fe));
     if (!ptr)
-      throw std::runtime_error("failed to malloc fe *");
+      throw std::bad_alloc();
     return ptr;
   };
 
