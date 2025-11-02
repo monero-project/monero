31fb9db894e146ddb37b18c097ae162bdb62a8cf
tobtoht <tob@featherwallet.org>
2025-11-02 15:22:55 +0100

    depends: freebsd_base: update to 15.1

--- contrib/depends/hosts/freebsd.mk
+++ contrib/depends/hosts/freebsd.mk
@@ -1,17 +1,19 @@
+FREEBSD_VERSION=15.0
+
 freebsd_CC=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
                -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
-               -u LIBRARY_PATH clang --target=$(host) --sysroot=$(build_prefix)/sysroot -iwithsysroot/usr/include
+               -u LIBRARY_PATH clang --target=$(host)$(FREEBSD_VERSION) --sysroot=$(build_prefix)/sysroot -iwithsysroot/usr/include
 freebsd_CXX=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
                 -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
-                -u LIBRARY_PATH clang++ --target=$(host) -stdlib=libc++ --sysroot=$(build_prefix)/sysroot \
+                -u LIBRARY_PATH clang++ --target=$(host)$(FREEBSD_VERSION) -stdlib=libc++ --sysroot=$(build_prefix)/sysroot \
                 -iwithsysroot/usr/include/c++/v1 -iwithsysroot/usr/include
 
 freebsd_AR=ar
 freebsd_RANLIB=ranlib
 freebsd_NM=nm
 
-freebsd_CFLAGS=-pipe -std=$(C_STANDARD)
-freebsd_CXXFLAGS=-pipe -std=$(CXX_STANDARD)
+freebsd_CFLAGS=-pipe -std=$(C_STANDARD) -Wl,-rpath-link,$(build_prefix)/sysroot/lib
+freebsd_CXXFLAGS=-pipe -std=$(CXX_STANDARD) -Wl,-rpath-link,$(build_prefix)/sysroot/lib
 freebsd_ARFLAGS=cr
 
 freebsd_release_CFLAGS=-O2
