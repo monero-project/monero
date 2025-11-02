16b19b417019d22dd440651c38fac1e29b0bdddb
tobtoht <tob@featherwallet.org>
2025-11-02 15:22:55 +0100

    depends: freebsd_base: update to 13.5

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -1,9 +1,9 @@
 package=freebsd_base
-$(package)_version=12.3
+$(package)_version=13.5
 $(package)_download_path=https://archive.freebsd.org/old-releases/amd64/$($(package)_version)-RELEASE/
 $(package)_download_file=base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
-$(package)_sha256_hash=e85b256930a2fbc04b80334106afecba0f11e52e32ffa197a88d7319cf059840
+$(package)_sha256_hash=5307c7699365834c9dd66216b774197af34c62b68fa9f526651c7081d8e1c5a5
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
