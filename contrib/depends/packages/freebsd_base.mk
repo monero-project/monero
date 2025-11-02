481e1fc34b15c44e3aa3611505ead4c50191da96
tobtoht <tob@featherwallet.org>
2025-11-02 15:22:55 +0100

    depends: freebsd_base: update to 15.0

--- contrib/depends/packages/freebsd_base.mk
+++ contrib/depends/packages/freebsd_base.mk
@@ -1,9 +1,9 @@
 package=freebsd_base
-$(package)_version=12.3
+$(package)_version=15.0
 $(package)_download_path=https://archive.freebsd.org/old-releases/amd64/$($(package)_version)-RELEASE/
 $(package)_download_file=base.txz
 $(package)_file_name=freebsd-base-$($(package)_version).txz
-$(package)_sha256_hash=e85b256930a2fbc04b80334106afecba0f11e52e32ffa197a88d7319cf059840
+$(package)_sha256_hash=ac0c933cc02ee8af4da793f551e4a9a15cdcf0e67851290b1e8c19dd6d30bba8
 
 define $(package)_extract_cmds
   echo $($(package)_sha256_hash) $($(1)_source_dir)/$($(package)_file_name) | sha256sum -c &&\
