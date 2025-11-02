d319f71e8aa8d909e6972c320bd52d1781d24284
tobtoht <tob@featherwallet.org>
2025-11-02 15:22:38 +0100

    depends: darwin_sdk: update to 26.1.1

--- contrib/depends/packages/darwin_sdk.mk
+++ contrib/depends/packages/darwin_sdk.mk
@@ -1,8 +1,8 @@
 package=darwin_sdk
-$(package)_version=12.2
+$(package)_version=26.1.1
 $(package)_download_path=https://bitcoincore.org/depends-sources/sdks
-$(package)_file_name=Xcode-12.2-12B45b-extracted-SDK-with-libcxx-headers.tar.gz
-$(package)_sha256_hash=df75d30ecafc429e905134333aeae56ac65fac67cb4182622398fd717df77619
+$(package)_file_name=Xcode-26.1.1-17B100-extracted-SDK-with-libcxx-headers.tar
+$(package)_sha256_hash=9600fa93644df674ee916b5e2c8a6ba8dacf631996a65dc922d003b98b5ea3b1
 
 # Prevent clang from including readline headers from the SDK. We statically link
 # our own version of readline.
