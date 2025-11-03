634496536a9ea6f441aced4c6e91a61c7557b9a5
jeffro256 <jeffro256@tutanota.com>
2025-11-03 17:05:57 -0600

    tx_memory_pool: speedup get_complement() for large requests
    Changes complexity from M*N to (2*N+M)*log2(M). The FCMP++ stressnet recently hit mempool sizes of ~55k txs.
    If the requesting node's mempool is populated, this results in an average of (55000*55000)/2
    (about 1.5 billion) comparisons for the responding node. Under this commit, this would be reduced to
    (55000+55000)*log2(55000) comparisons (about 2.6 million), a 99.83% reduction.

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -671,12 +671,27 @@ namespace cryptonote
     CRITICAL_REGION_LOCAL(m_transactions_lock);
     CRITICAL_REGION_LOCAL1(m_blockchain);
 
-    m_blockchain.for_all_txpool_txes([this, &hashes, &txes](const crypto::hash &txid, const txpool_tx_meta_t &meta, const cryptonote::blobdata_ref*) {
+    // Create an ordering for `crypto::hash` and sort `hashes` according to that order
+    struct hash_compare
+    {
+      bool operator()(const crypto::hash &lhs, const crypto::hash &rhs) const noexcept
+      {
+        return memcmp(lhs.data, rhs.data, sizeof(crypto::hash)) < 0;
+      }
+    };
+    std::vector<crypto::hash> hashes_sorted(hashes.cbegin(), hashes.cend());
+    std::sort(hashes_sorted.begin(), hashes_sorted.end(), hash_compare{});
+
+    m_blockchain.for_all_txpool_txes([this, &hashes_sorted, &txes](const crypto::hash &txid, const txpool_tx_meta_t &meta, const cryptonote::blobdata_ref*) {
       const auto tx_relay_method = meta.get_relay_method();
       if (tx_relay_method != relay_method::block && tx_relay_method != relay_method::fluff)
         return true;
-      const auto i = std::find(hashes.begin(), hashes.end(), txid);
-      if (i == hashes.end())
+
+      // Do binary search for our pool TXID in given list, skip to next if already present
+      const auto hash_it = std::lower_bound(hashes_sorted.cbegin(), hashes_sorted.cend(), txid, hash_compare{});
+      if (hash_it != hashes_sorted.cend() && *hash_it == txid)
+        return true;
+
       {
         cryptonote::blobdata bd;
         try
