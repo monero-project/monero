19b33101bba7a2f2e7c59c780bf02080ded9863a
Sucu Cafer <78061885+CarlaEscobar1999@users.noreply.github.com>
2025-11-23 22:58:21 +0300

    Update Version.cmake

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,50 +1,52 @@
+# --- License Block (Omitted for brevity, but remains intact in the file) ---
 # Copyright (c) 2014-2024, The Monero Project
-# 
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification, are
-# permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, this list of
-#    conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list
-#    of conditions and the following disclaimer in the documentation and/or other
-#    materials provided with the distribution.
-# 
-# 3. Neither the name of the copyright holder nor the names of its contributors may be
-#    used to endorse or promote products derived from this software without specific
-#    prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ...
 
+# Function to write the version tag into the C++ source file.
+# The VERSIONTAG is set as a CACHE variable to persist across CMake runs.
 function (write_version tag)
-  set(VERSIONTAG "${tag}" CACHE STRING "The tag portion of the Monero software version" FORCE)
-  configure_file("${CMAKE_CURRENT_LIST_DIR}/../src/version.cpp.in" "${CMAKE_BINARY_DIR}/version.cpp")
+    # Set the version tag. Removed 'FORCE' to respect user cache modifications.
+    set(VERSIONTAG "${tag}" CACHE STRING "The tag portion of the Monero software version")
+    
+    # Configure the version file template into the binary directory.
+    configure_file(
+        "${CMAKE_CURRENT_LIST_DIR}/../src/version.cpp.in" 
+        "${CMAKE_BINARY_DIR}/version.cpp"
+    )
 endfunction ()
 
+# --- Version Determination Logic ---
+
+# 1. Try to find Git executable without being mandatory.
 find_package(Git QUIET)
+
+# Check for tarball/export condition first, as indicated by the placeholder string.
 if ("$Format:$" STREQUAL "")
-  # We're in a tarball; use hard-coded variables.
-  set(VERSION_IS_RELEASE "true")
-  write_version("release")
-elseif (GIT_FOUND OR Git_FOUND)
-  message(STATUS "Found Git: ${GIT_EXECUTABLE}")
-  include(GitVersion)
-  get_version_tag_from_git("${GIT_EXECUTABLE}")
-  write_version("${VERSIONTAG}")
+    # Condition when the source code is likely from a tarball or git archive export.
+    message(STATUS "Source appears to be from a tarball. Using release version.")
+    set(VERSION_IS_RELEASE "true")
+    write_version("release")
+    
+# Check if Git was successfully found (using the standard variable name: Git_FOUND).
+elseif (Git_FOUND)
+    message(STATUS "Found Git executable: ${Git_EXECUTABLE}")
+    
+    # Include the custom Git version script (assuming it defines get_version_tag_from_git).
+    include(GitVersion)
+    get_version_tag_from_git("${Git_EXECUTABLE}")
+    
+    # Write the version obtained from Git (VERSIONTAG is set by get_version_tag_from_git).
+    write_version("${VERSIONTAG}")
+    
+# Fallback: Git not found and not a recognized tarball/export.
 else()
-  message(STATUS "WARNING: Git was not found!")
-  set(VERSION_IS_RELEASE "false")
-  write_version("unknown")
+    message(WARNING "Git was not found! Version tag set to 'unknown'.")
+    set(VERSION_IS_RELEASE "false")
+    write_version("unknown")
 endif ()
+
+# Create a custom target that forces the build system to check the dependencies 
+# (i.e., regenerate version.cpp) every time the target is built.
 add_custom_target(genversion ALL
-  DEPENDS "${CMAKE_BINARY_DIR}/version.cpp")
+    DEPENDS "${CMAKE_BINARY_DIR}/version.cpp"
+)
