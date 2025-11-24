4741792dc8f2de9ac64b27dc07a42eaec8515580
tobtoht <tob@featherwallet.org>
2025-11-24 22:39:55 +0100

    debug_utilities: fix build with Boost ASIO 1.87+

--- src/debug_utilities/object_sizes.cpp
+++ src/debug_utilities/object_sizes.cpp
@@ -70,8 +70,8 @@ int main(int argc, char* argv[])
   mlog_configure("", true);
 
   SL(boost::thread);
-  SL(boost::asio::io_service);
-  SL(boost::asio::io_service::work);
+  SL(boost::asio::io_context);
+  SL(boost::asio::executor_work_guard<boost::asio::io_context::executor_type>);
   SL(boost::asio::deadline_timer);
 
   SL(cryptonote::DB_ERROR);
