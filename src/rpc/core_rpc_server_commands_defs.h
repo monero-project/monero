9dbf7284f6bd5e406a086a78019faa82a0c301f5
Navid Rahimi <rahimi.nv@gmail.com>
2026-03-03 02:07:07 +0000

    wallet: adaptive batch sizing for get_transactions on unrestricted daemons

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -79,6 +79,7 @@ namespace cryptonote
 #define CORE_RPC_STATUS_BUSY   "BUSY"
 #define CORE_RPC_STATUS_NOT_MINING "NOT MINING"
 #define CORE_RPC_STATUS_PAYMENT_REQUIRED "PAYMENT REQUIRED"
+#define CORE_RPC_STATUS_TOO_LARGE "TOO LARGE"
 
 inline const std::string get_rpc_status(const bool trusted_daemon, const std::string &s)
 {
@@ -89,6 +90,8 @@ inline const std::string get_rpc_status(const bool trusted_daemon, const std::st
   if (s == CORE_RPC_STATUS_BUSY)
     return s;
   if (s == CORE_RPC_STATUS_PAYMENT_REQUIRED)
+    return s;
+  if (s == CORE_RPC_STATUS_TOO_LARGE)
     return s;
   return "<error>";
 }
@@ -101,7 +104,7 @@ inline const std::string get_rpc_status(const bool trusted_daemon, const std::st
 // advance which version they will stop working with
 // Don't go over 32767 for any of these
 #define CORE_RPC_VERSION_MAJOR 3
-#define CORE_RPC_VERSION_MINOR 16
+#define CORE_RPC_VERSION_MINOR 17
 #define MAKE_CORE_RPC_VERSION(major,minor) (((major)<<16)|(minor))
 #define CORE_RPC_VERSION MAKE_CORE_RPC_VERSION(CORE_RPC_VERSION_MAJOR, CORE_RPC_VERSION_MINOR)
 
