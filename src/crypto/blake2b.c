d8f27be05fafad95e88bf500e7f7fef0f36d0cae
tobtoht <tob@featherwallet.org>
2026-04-28 13:33:08 +0200

    fix: warning: unused-function

--- src/crypto/blake2b.c
+++ src/crypto/blake2b.c
@@ -66,21 +66,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
  /* Argon2 Team - End Code */
 
-static FORCE_INLINE uint32_t load32(const void *src) {
-#if defined(NATIVE_LITTLE_ENDIAN)
-	uint32_t w;
-	memcpy(&w, src, sizeof w);
-	return w;
-#else
-	const uint8_t *p = (const uint8_t *)src;
-	uint32_t w = *p++;
-	w |= (uint32_t)(*p++) << 8;
-	w |= (uint32_t)(*p++) << 16;
-	w |= (uint32_t)(*p++) << 24;
-	return w;
-#endif
-}
-
 static FORCE_INLINE uint64_t load64_native(const void *src) {
 	uint64_t w;
 	memcpy(&w, src, sizeof w);
@@ -149,36 +134,6 @@ static FORCE_INLINE void store64(void *dst, uint64_t w) {
 /// END: endian.h
 
 /// BEGIN: blake2-impl.h
-
-static FORCE_INLINE uint64_t load48(const void *src) {
-	const uint8_t *p = (const uint8_t *)src;
-	uint64_t w = *p++;
-	w |= (uint64_t)(*p++) << 8;
-	w |= (uint64_t)(*p++) << 16;
-	w |= (uint64_t)(*p++) << 24;
-	w |= (uint64_t)(*p++) << 32;
-	w |= (uint64_t)(*p++) << 40;
-	return w;
-}
-
-static FORCE_INLINE void store48(void *dst, uint64_t w) {
-	uint8_t *p = (uint8_t *)dst;
-	*p++ = (uint8_t)w;
-	w >>= 8;
-	*p++ = (uint8_t)w;
-	w >>= 8;
-	*p++ = (uint8_t)w;
-	w >>= 8;
-	*p++ = (uint8_t)w;
-	w >>= 8;
-	*p++ = (uint8_t)w;
-	w >>= 8;
-	*p++ = (uint8_t)w;
-}
-
-static FORCE_INLINE uint32_t rotr32(const uint32_t w, const unsigned c) {
-	return (w >> c) | (w << (32 - c));
-}
 
 static FORCE_INLINE uint64_t rotr64(const uint64_t w, const unsigned c) {
 	return (w >> c) | (w << (64 - c));
