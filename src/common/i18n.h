74e03c43d43a0570c133d9deccad988b289c11bd
tobtoht <tob@featherwallet.org>
2026-05-28 15:19:56 +0200

    remove translations

--- src/common/i18n.h
+++ src/common/i18n.h
@@ -28,12 +28,5 @@
 
 #pragma once
 
-#include <string>
-
-#define QT_TRANSLATE_NOOP(context,str) i18n_translate(str,context)
-
-std::string i18n_get_language();
-int i18n_set_language(const char *directory, const char *base, std::string language = std::string());
-const char *i18n_translate(const char *str, const std::string &context);
-static inline std::string get_default_i18n_context() { return std::string(); }
-static inline const char *tr(const char *str) { return i18n_translate(str,get_default_i18n_context()); }
+inline const char *i18n_translate(const char *str, const std::string &context) { return str; };
+static inline const char *tr(const char *str) { return str; }
