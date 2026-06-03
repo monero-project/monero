ab6a1c8a3da2b252599fcb72c16f26fa56ba4cc8
SChernykh <15806605+SChernykh@users.noreply.github.com>
2026-06-03 20:47:05 +0200

    tx_pool: fix use-after-free in prune() - txid was a reference to an item which was later deleted in remove_tx_from_transient_lists(), and txid was used after that

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -416,7 +416,7 @@ namespace cryptonote
         break;
       try
       {
-        const crypto::hash &txid = it->second;
+        const crypto::hash txid = it->second;
         txpool_tx_meta_t meta;
         if (!m_blockchain.get_txpool_tx_meta(txid, meta))
         {
