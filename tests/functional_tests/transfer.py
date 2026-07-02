fcd5b3f0e6254763eed1641019d9f06b98a1c861
selsta <selsta@sent.at>
2026-07-03 01:08:19 +0200

    functional_tests: avoid sweep_single spent status race

--- tests/functional_tests/transfer.py
+++ tests/functional_tests/transfer.py
@@ -684,6 +684,8 @@ class TransferTest():
         res = self.wallet[0].sweep_single('44Kbx4sJ7JDRDV5aAhLJzQCjDz2ViLRduE3ijDZu3osWKBjMGkV1XPk4pfDUMqt1Aiezvephdqm6YD19GKFD9ZcXVUTp6BW', key_image = ki)
         assert len(res.tx_hash) == 64
         tx_hash = res.tx_hash
+        # Avoid racing the txpool relay state update before checking spent status
+        time.sleep(0.1)
         res = daemon.is_key_image_spent([ki])
         assert len(res.spent_status) == 1
         assert res.spent_status[0] == 2
