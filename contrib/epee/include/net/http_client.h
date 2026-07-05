de19ff65c9441a124744b5f7f3bbe12e0b228e8b
nahuhh <50635951+nahuhh@users.noreply.github.com>
2026-07-05 21:30:34 +0000

    wallet: persist set_daemon params across wallet loads.     - allow set_daemon without a wallet loaded

--- contrib/epee/include/net/http_client.h
+++ contrib/epee/include/net/http_client.h
@@ -124,10 +124,10 @@ namespace net_utils
 			{
 				CRITICAL_REGION_LOCAL(m_lock);
 				disconnect();
+				m_net_client.set_ssl(std::move(ssl_options));
 				m_host_buff = std::move(host);
 				m_port = std::move(port);
 				m_auth = user ? http_client_auth{std::move(*user)} : http_client_auth{};
-				m_net_client.set_ssl(std::move(ssl_options));
 			}
 
 			void set_auto_connect(bool auto_connect) override
