0e0a62d8f821eeb08e30d5860352887aceb08317
jpk68 <jpk68@tutanota.com>
2026-08-07 21:22:37 -0400

    src, epee: decouple net client users from http_client header
    Co-authored-by: mj-xmr <mjxmr@protonmail.com>

--- src/device_trezor/trezor/transport.hpp
+++ src/device_trezor/trezor/transport.hpp
@@ -38,8 +38,10 @@
 
 #include <typeinfo>
 #include <type_traits>
-#include "net/http_client.h"
 #include "scope_guard.h"
+#include "net/abstract_http_client.h"
+#include "misc_language.h"
+#include "misc_log_ex.h"
 
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
@@ -53,6 +55,10 @@
 #include "messages/messages-common.pb.h"
 #include "messages/messages-management.pb.h"
 #include "messages/messages-monero.pb.h"
+
+#ifdef WITH_DEVICE_TREZOR_WEBUSB
+#include <libusb.h>
+#endif
 
 namespace hw {
 namespace trezor {
@@ -192,7 +198,7 @@ namespace trezor {
     std::ostream& dump(std::ostream& o) const override;
 
   private:
-    epee::net_utils::http::http_simple_client m_http_client;
+    const std::unique_ptr<epee::net_utils::http::abstract_http_client> m_http_client;
     std::string m_bridge_host;
     boost::optional<std::string> m_device_path;
     boost::optional<std::string> m_session;
@@ -251,8 +257,6 @@ namespace trezor {
   };
 
 #ifdef WITH_DEVICE_TREZOR_WEBUSB
-#include <libusb.h>
-
   class WebUsbTransport : public Transport {
   public:
 
