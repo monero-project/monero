88bdf77e7e37234f0353873386f5c0830bce50d0
Jackie <pyxmr2025@openenet.cn>
2026-09-12 16:10:50 +0800

    rpc_args: add rpc_auth command line

--- src/rpc/rpc_args.h
+++ src/rpc/rpc_args.h
@@ -68,6 +68,7 @@ namespace cryptonote
       const command_line::arg_descriptor<bool> rpc_ssl_allow_chained;
       const command_line::arg_descriptor<bool> rpc_ssl_allow_any_cert;
       const command_line::arg_descriptor<bool> disable_rpc_ban;
+      const command_line::arg_descriptor<std::string> rpc_auth;
     };
 
     // `allow_any_cert` bool toggles `--rpc-ssl-allow-any-cert` configuration
