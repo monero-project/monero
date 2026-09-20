fb7e0d7bf88358ad22820b0ce278e608b39bc1cf
Jackie <pyxmr2025@openenet.cn>
2026-09-21 07:22:01 +0800

    to run ci

--- contrib/epee/include/net/http_auth.h
+++ contrib/epee/include/net/http_auth.h
@@ -26,7 +26,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
-
+ 
 #include <boost/optional/optional.hpp>
 #include <boost/utility/string_ref.hpp>
 #include <cstdint>
